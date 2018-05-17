64c986cc112d05afdf38504747927f3f707291b0
Jérémy Buget <jbuget@gmail.com>
2018-05-17 22:24:25 +0200

    Refactor route faq/get

--- app/routes/faq/get.js
+++ app/routes/faq/get.js
@@ -6,16 +6,20 @@ export default Route.extend({
   prismic: service(),
 
   async model(params) {
-    const faqCategoryUID = params.uid;
-    const api = await this.get('prismic').getApi();
-    const faqMenu = (await api.query(
-      Prismic.Predicates.at('document.type', 'faq_menu'),
-      { 'fetchLinks': 'faq_category.title' }))
-      .results[0];
-    const faqItems = (await api.query(
-      Prismic.Predicates.at('my.faq_category.uid', faqCategoryUID),
-      { 'fetchLinks': ['faq_item.uid', 'faq_item.question', 'faq_item.answer'] }))
-      .results[0];
+    const faqMenu = await this._fetchFaqMenuDocument();
+    const faqItems = await this._fetchFaqItems(params.uid);
     return { faqMenu, faqItems };
+  },
+
+  async _fetchFaqMenuDocument() {
+    const api = await this.get('prismic').getApi();
+    const document = await api.query(Prismic.Predicates.at('document.type', 'faq_menu'), { 'fetchLinks': 'faq_category.title' });
+    return document.results[0];
+  },
+
+  async _fetchFaqItems(faqCategoryUid) {
+    const api = await this.get('prismic').getApi();
+    const document = await api.query(Prismic.Predicates.at('my.faq_category.uid', faqCategoryUid), { 'fetchLinks': ['faq_item.uid', 'faq_item.question', 'faq_item.answer'] });
+    return document.results[0];
   }
 });
