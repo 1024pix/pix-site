dbe26c2332b2849f2f73586cadecaa3dcf339f7d
Jérémy Buget <jbuget@gmail.com>
2018-05-20 09:29:14 +0200

    Add tests on component app-header

--- tests/integration/components/app-header-test.js
+++ tests/integration/components/app-header-test.js
@@ -4,23 +4,37 @@ import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | app-header', function(hooks) {
+
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
+    // when
     await render(hbs`{{app-header}}`);
 
+    // then
     assert.equal(this.element.textContent.trim(), '');
+  });
 
-    // Template block usage:
-    await render(hbs`
-      {{#app-header}}
-        template block text
-      {{/app-header}}
-    `);
+  test('it contains the logo to Pix.fr', async function(assert) {
+    // when
+    await render(hbs`{{app-header}}`);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    // then
+    const $appLogoPixLink = this.element.querySelector('a.app-logo-pix__link');
+    assert.equal($appLogoPixLink.getAttribute('href'), 'https://pix.fr');
+
+    const $appLogoPixImage = $appLogoPixLink.querySelector('img.app-logo-pix__image');
+    assert.equal($appLogoPixImage.getAttribute('src'), '/logo-pix.png');
+    assert.equal($appLogoPixImage.getAttribute('alt'), 'Revenir sur le site Pix.fr');
+  });
+
+  test('it contains the logo of the French Republic', async function(assert) {
+    // when
+    await render(hbs`{{app-header}}`);
+
+    // then
+    const $appLogoPixMarianne = this.element.querySelector('img.app-logo-marianne__image');
+    assert.equal($appLogoPixMarianne.getAttribute('src'), '/logo-marianne.svg');
+    assert.equal($appLogoPixMarianne.getAttribute('alt'), 'Logo de la République Française');
   });
 });
