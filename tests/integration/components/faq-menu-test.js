456d346a7d7b7bcc18f1b38e9ced4bde2daaf9ca
Jérémy Buget <jbuget@gmail.com>
2018-05-21 15:58:09 +0200

    Add tests for component faq-item (with qunit-dom NPM dependency)

--- tests/integration/components/faq-menu-test.js
+++ tests/integration/components/faq-menu-test.js
@@ -7,11 +7,43 @@ module('Integration | Component | faq-menu', function(hooks) {
 
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
+  const faqMenu = {
+    rawJSON: {
+      faq_categories: [{
+        faq_category: {
+          uid: 'faq_category_a',
+          data: {
+            title: [{
+              text: 'FAQ category A',
+              type: 'heading1'
+            }]
+          }
+        }
+      }, {
+        faq_category: {
+          uid: 'faq_category_b',
+          data: {
+            title: [{
+              text: 'FAQ category B',
+              type: 'heading1'
+            }]
+          }
+        }
+      }]
+    }
+  };
+
+  test('it renders as many entries as categories fetched', async function(assert) {
+    // given
+    this.set('faqMenu', faqMenu);
+
     // when
-    await render(hbs`{{faq-menu}}`);
+    await render(hbs`{{faq-menu document=faqMenu}}`);
 
     // then
-    assert.equal(this.element.textContent.trim(), '');
+    const $menuEntries = this.element.querySelectorAll('.faq-menu-entry .faq-menu-entry__link');
+    assert.equal(this.element.querySelectorAll('li.faq-menu-entry a.faq-menu-entry__link').length, 2);
+    assert.equal($menuEntries[0].textContent.trim(), 'FAQ category A');
+    assert.equal($menuEntries[1].textContent.trim(), 'FAQ category B');
   });
 });
