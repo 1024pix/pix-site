6bbc52c9ccec68a6dc715ce7b04cd2a4168f8cbb
Jérémy Buget <jbuget@gmail.com>
2018-08-23 09:52:00 +0200

    Add page index with new hero image

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,4 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-
-  beforeModel() {
-    return this.transitionTo('faq');
-  }
 });
--- tests/unit/routes/index-test.js
+++ tests/unit/routes/index-test.js
@@ -2,7 +2,6 @@ import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
 module('Unit | Route | index', function(hooks) {
-
   setupTest(hooks);
 
   test('it exists', function(assert) {
