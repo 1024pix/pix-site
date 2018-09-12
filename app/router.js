817a6a269cb1910f915561ff5f1216bd9ca36e77
Benoit <benoit.prady@pix.fr>
2018-09-12 12:15:19 +0200

    Remove route for certification center elibility form

--- app/router.js
+++ app/router.js
@@ -34,7 +34,6 @@ Router.map(function() {
     this.route('school-education-establishement-registration', { path: 'inscription-etablissement-scolaire' });
     this.route('higher-education-establishement-registration', { path: 'inscription-etablissement-superieur' });
     this.route('pix-orga-newsletter-subscription', { path: 'abonnement-actualites-pix-orga' });
-    this.route('certification-center-eligibility', { path: 'eligibilite-centre-de-certification' });
     this.route('pix-orga-registration', { path: 'inscription-pix-orga' });
   });
   this.route('certifications');
