507811dfec8da98ccd577beb40d5388717c21a0f
Jonathan Perret <jonathan@ut7.fr>
2018-10-01 18:13:46 +0200

    Exclude logo-pix_250x250.png from fingerprinting

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    fingerprint: {
+      exclude: ['images/logo-pix_250x250.png'],
+    }
   });
 
 
