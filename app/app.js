5d6afd94bd8a9720cda6ac9260b6338b8c3b49eb
David Hérault <dherault@gmail.com>
2019-05-14 10:43:09 +0200

    Add promise librairy for Prismic

--- app/app.js
+++ app/app.js
@@ -1,7 +1,11 @@
+import RSVP from 'rsvp'
 import Application from '@ember/application';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
+
+// Prismic requires a Promise implementation, undefined under IE
+window.Promise = RSVP.Promise;
 
 const App = Application.extend({
   modulePrefix: config.modulePrefix,
