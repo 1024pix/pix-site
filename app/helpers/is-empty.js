8ecf310ee42755f7202d89f73889bdb19b66b33a
Jérémy Buget <jbuget@gmail.com>
2019-05-14 12:23:36 +0200

    Fix simple-page rendering problem when header, body or footer is empty.

--- app/helpers/is-empty.js
+++ app/helpers/is-empty.js
@@ -2,7 +2,7 @@ import { helper } from '@ember/component/helper';
 import { isEmpty } from '@ember/utils';
 
 export default helper(function ([fragment]) {
-  if (!fragment) return true;
+  if (!fragment || fragment.length === 0) return true;
   if (fragment.length > 1) return false;
   return isEmpty(fragment[0].text);
 })
