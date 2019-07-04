e08ed942fe48c3358e2d547f23b866e5b2d13e31
Vincent Hardouin <vincent.hardouin@pix.fr>
2019-07-04 17:18:55 +0200

    Extract prismic configuration into environment variables.
    - add "ember-cli-dotenv" dependency
    - declare prismic API variable
    - configure dotenv to fail when missing variables

--- config/dotenv.js
+++ config/dotenv.js
@@ -1,16 +1,9 @@
 'use strict';
 
-<<<<<<< HEAD
-module.exports = function() {
-  return {
-    clientAllowedKeys: ['PRISMIC_API_ENDPOINT', 'PRISMIC_API_TOKEN'],
-=======
-const path = require('path');
 
 module.exports = function() {
   return {
-    clientAllowedKeys: ['PRISMIC_API_TOKEN'],
->>>>>>> Add environment variables
+    clientAllowedKeys: ['PRISMIC_API_ENDPOINT', 'PRISMIC_API_TOKEN'],
     failOnMissingKey: false,
   }
 };
