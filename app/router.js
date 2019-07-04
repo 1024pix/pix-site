e72b9092bf2ac103f8af90adf1faf1746798cae7
David Hérault <dherault@gmail.com>
2019-07-04 15:21:05 +0200

    Add ember-cli-matomo-tag-manager

--- app/router.js
+++ app/router.js
@@ -1,28 +1,16 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
-import { inject as service } from '@ember/service';
-import { scheduleOnce } from '@ember/runloop';
 
 const Router = EmberRouter.extend({
   location: config.locationType,
   rootURL: config.rootURL,
-  metrics: service(),
 
   init() {
     this._super(...arguments);
     this.on('routeDidChange', () => {
-      this._trackPage();
       window.scrollTo(0, 0);
     });
   },
-
-  _trackPage() {
-    scheduleOnce('afterRender', this, () => {
-      const page = this.url;
-      const title = this.getWithDefault('currentRouteName', 'unknown');
-      this.metrics.trackPage({ page, title });
-    });
-  }
 });
 
 Router.map(function() {
