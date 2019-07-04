56c8fbb0f73ae5393b298e62c4f7c5164e4fa20a
Vincent Hardouin <vincent.hardouin@pix.fr>
2019-07-04 15:39:44 +0200

    Add environment variables
    for prismic api

--- config/dotenv.js
+++ config/dotenv.js
@@ -1,8 +1,16 @@
 'use strict';
 
+<<<<<<< HEAD
 module.exports = function() {
   return {
     clientAllowedKeys: ['PRISMIC_API_ENDPOINT', 'PRISMIC_API_TOKEN'],
+=======
+const path = require('path');
+
+module.exports = function() {
+  return {
+    clientAllowedKeys: ['PRISMIC_API_TOKEN'],
+>>>>>>> Add environment variables
     failOnMissingKey: false,
   }
 };
