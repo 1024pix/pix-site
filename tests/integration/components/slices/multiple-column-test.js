73e87d91157508da9d8c23ece403ad3948af2060
Vincent Hardouin <vincent.hardouin@pix.fr>
2019-07-08 18:05:07 +0200

    Add slice-zone and multiple-column slice
    - Add slice-zone for manage slice
    - Add multiple-column slice
    - Readjust web-snippet for slice-zone

---
+++ tests/integration/components/slices/multiple-column-test.js
@@ -0,0 +1,65 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | slices/multiple-column', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it render', async function(assert) {
+    await render(hbs `{{slices/multiple-column}}`);
+    assert.equal(this.element.textContent.trim(), '');
+  });
+
+  test('it should have columns', async function (assert) {
+    //given
+    this.set('slice', {
+      "slice_type": "multiple_column",
+      "slice_label": null,
+      "items": [
+        {
+          "column_content": [
+            {
+              "type": "paragraph",
+              "text": "Column 1",
+              "spans": []
+            }
+          ]
+        },
+        {
+          "column_content": [
+            {
+              "type": "paragraph",
+              "text": "Column 2",
+              "spans": []
+            }
+          ]
+        },
+        {
+          "column_content": [
+            {
+              "type": "image",
+              "url": "https://pix-site.cdn.prismic.io/pix-site/3a29bf3168b1357d462651dde9526d427e98eced_recruitment.jpg",
+              "alt": null,
+              "copyright": null,
+              "dimensions": {
+                "width": 350,
+                "height": 200
+              }
+            }
+          ]
+        }
+      ],
+      "primary": {}
+    });
+
+    //when
+    await render(hbs `{{slices/multiple-column slice=slice}}`);
+
+    //then
+    assert.dom('.multiple-column>.column:first-child').hasText('Column 1');
+    assert.dom('.multiple-column>.column:nth-child(2)').hasText('Column 2');
+    assert.dom('.multiple-column>.column:nth-child(3)>.block-img').exists(); 
+  
+  });
+});
