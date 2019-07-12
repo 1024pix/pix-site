0ae56b4d4aae88972737fb66ec650c1d8367253f
Vincent Hardouin <vincent.hardouin@pix.fr>
2019-07-12 18:08:05 +0200

    Add destroy chart

--- app/components/ember-chart.js
+++ app/components/ember-chart.js
@@ -1,5 +1,6 @@
 import Component from '@ember/component';
 import { Chart } from 'chart.js';
+import ENV from 'pix-site/config/environment';
 
 export default Component.extend({
 
@@ -11,14 +12,21 @@ export default Component.extend({
   data: null,
   width: null,
   height: null,
+  chart: null,
 
   didInsertElement() {
     this._super(...arguments);
     if (this.data !== null) {
-      new Chart(document.getElementById(this.elementId).children, {
+      this.chart = new Chart(document.getElementById(this.elementId).children, {
         type: this.type,
         data: this.data,
       });
     }
-  }
+  },
+
+  didDestroyElement() {
+    if (!ENV.environnement === 'test') {
+      this.chart.destroy();
+    }
+  },
 });
