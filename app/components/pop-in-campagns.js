1e3f23f62708bb126ac12d770da4cbbcf3f24463
Vincent Hardouin <vincent.hardouin@pix.fr>
2019-07-30 10:49:27 +0200

    Refacto css

--- app/components/pop-in-campagns.js
+++ app/components/pop-in-campagns.js
@@ -1,5 +1,17 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNames: ['pop-in-campagns'],
+  classNames: ['pop-in-campagns','pop-in-campagns__bottom'],
+  classNameBindings: ['isClose:pop-in-campagns-min'],
+  isClose: null,
+
+
+  actions: {
+    closePopIn() {
+      this.set('isClose', true);
+    },
+    openPopIn() {
+      this.set('isClose', false);
+    },
+  }
 });
--- tests/integration/components/pop-in-campagns-test.js
+++ tests/integration/components/pop-in-campagns-test.js
@@ -7,20 +7,9 @@ module('Integration | Component | pop-in-campagns', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
+    //when
     await render(hbs`<PopInCampagns />`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      <PopInCampagns>
-        template block text
-      </PopInCampagns>
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    //then
+    assert.dom('.pop-in-campagns').exists();
   });
 });
