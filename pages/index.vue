828c7d93001bbadde85ad15aec0a0b946cc7ba49
Vincent Hardouin <vincent.hardouin@pix.fr>
2019-09-16 17:56:49 +0200

    Add the organization navigation bar

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,40 @@
 <template>
-  <div class="container">
-    <div>
-      <!-- Template for page title -->
-      <h1 class="page-title">
-        Pix-site V2
-      </h1>
-    </div>
+  <div class="index">
+    <header>
+      <organization-nav :organizationNavItems="organizationNavItems" />
+    </header>
+    <div></div>
   </div>
 </template>
 
 <script>
+import Prismic from 'prismic-javascript'
+import PrismicConfig from '~/prismic.config.js'
+import OrganizationNav from '~/components/organization-nav'
+
 export default {
-  components: {}
+  components: {
+    OrganizationNav
+  },
+
+  async asyncData({ context, error, req }) {
+    try {
+      const api = await Prismic.getApi(PrismicConfig.apiEndpoint, { req })
+      const response = await api.getSingle('navigation')
+      const organizationNavItems = response.data.body.filter(
+        (body) => body.primary.type === 'organizations-nav'
+      )
+      return {
+        organizationNavItems
+      }
+    } catch (e) {
+      error({ statusCode: 404, message: 'Page not found' })
+    }
+  }
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .container {
   width: 1024px;
   margin: 0 auto;
