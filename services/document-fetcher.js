764e0f4fa13298db0bbcdfb0216f7455d38f7544
Vincent Hardouin <vincent.hardouin@pix.fr>
2019-09-17 16:33:50 +0200

    Add possibility to choose language in document-fetcher

--- services/document-fetcher.js
+++ services/document-fetcher.js
@@ -1,9 +1,11 @@
 import Prismic from 'prismic-javascript'
 import PrismicConfig from '~/prismic.config.js'
 
-export default {
-  getNavigation: async () => {
-    const api = await Prismic.getApi(PrismicConfig.apiEndpoint)
-    return api.getSingle('navigation')
+export default function(lang = 'fr-fr') {
+  return {
+    getNavigation: async () => {
+      const api = await Prismic.getApi(PrismicConfig.apiEndpoint)
+      return api.getSingle('navigation', { lang })
+    }
   }
 }
--- tests/services/document-fetcher.test.js
+++ tests/services/document-fetcher.test.js
@@ -12,7 +12,7 @@ describe('DocumentFetcher', () => {
     }
     Prismic.getApi.mockResolvedValue(prismicApi)
     // When
-    await DocumentFetcher.getNavigation()
+    await DocumentFetcher().getNavigation()
 
     // Then
     expect(Prismic.getApi).toBeCalledWith(PrismicConfig.apiEndpoint)
@@ -25,9 +25,21 @@ describe('DocumentFetcher', () => {
     }
     Prismic.getApi.mockResolvedValue(prismicApi)
     // When
-    const navigation = await DocumentFetcher.getNavigation()
+    const navigation = await DocumentFetcher('en-gb').getNavigation()
     // Then
-    expect(prismicApi.getSingle).toBeCalledWith('navigation')
+    expect(prismicApi.getSingle).toBeCalledWith('navigation', { lang: 'en-gb' })
     expect(navigation.uid).toBe('navigation')
+  })
+
+  test('it should get navigation from Prismic for French when language is not defined', async () => {
+    // Given
+    const prismicApi = {
+      getSingle: jest.fn(async () => ({ uid: 'navigation' }))
+    }
+    Prismic.getApi.mockResolvedValue(prismicApi)
+    // When
+    await DocumentFetcher().getNavigation()
+    // Then
+    expect(prismicApi.getSingle).toBeCalledWith('navigation', { lang: 'fr-fr' })
   })
 })
