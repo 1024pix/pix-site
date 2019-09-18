b0b56ac0e869ff0194f6477f6f869b7bf7a20442
Vincent Hardouin <vincent.hardouin@pix.fr>
2019-09-18 10:05:41 +0200

    Handle language selection in Document Fetcher

--- services/document-fetcher.js
+++ services/document-fetcher.js
@@ -1,11 +1,12 @@
 import Prismic from 'prismic-javascript'
 import PrismicConfig from '~/prismic.config.js'
 
-export default function(lang = 'fr-fr') {
+export default function(i18n = { defaultLocale: 'fr-fr' }) {
+  const language = i18n.locale || i18n.defaultLocale
   return {
     getNavigation: async () => {
       const api = await Prismic.getApi(PrismicConfig.apiEndpoint)
-      return api.getSingle('navigation', { lang })
+      return api.getSingle('navigation', { lang: language })
     }
   }
 }
