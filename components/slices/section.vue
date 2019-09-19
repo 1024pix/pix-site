ec16be86b999325383ef72193984a00e2bc58992
Guillaume Lagorce <glagorce@octo.com>
2019-09-19 12:17:19 +0200

    Create Section and SectionColumn components and use them in digital mediation page

---
+++ components/slices/section.vue
@@ -0,0 +1,57 @@
+<template>
+  <section :class="sectionClass">
+    <div class="background"></div>
+    <div class="container padding-container">
+      <prismic-rich-text :field="title" />
+      <prismic-rich-text :field="description" />
+      <div class="features-wrapper">
+        <ul :class="ulClass">
+          <li
+            v-for="(item, index) in paragraphs"
+            :key="`item-${index}`"
+            :class="liClass"
+          >
+            <prismic-rich-text :field="item.paragraph" />
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'SectionSlice',
+  props: {
+    content: {
+      type: Object,
+      default: null
+    },
+    sectionClass: {
+      type: String,
+      default: ''
+    },
+    ulClass: {
+      type: String,
+      default: ''
+    },
+    liClass: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    paragraphs() {
+      return this.content.items
+    },
+    title() {
+      return this.content.primary.title
+    },
+    description() {
+      return this.content.primary.description
+    }
+  }
+}
+</script>
+
+<style></style>
--- pages/digital-mediation.vue
+++ pages/digital-mediation.vue
@@ -4,92 +4,47 @@
       <div class="background"></div>
       <div class="container padding-container">
         <prismic-rich-text :field="document[0].primary.title" />
-        <a class="btn" :href="document[0].items[0].link_button">{{
-          $prismic.richTextAsPlain(document[0].items[0].title_button)
-        }}</a>
+        <a class="btn" :href="document[0].items[0].link_button">
+          {{ $prismic.richTextAsPlain(document[0].items[0].title_button) }}
+        </a>
       </div>
     </section>
 
-    <section class="partnerships">
-      <div class="background"></div>
-      <div class="container padding-container">
-        <prismic-rich-text :field="document[1].primary.title" />
-        <ul class="partners">
-          <li
-            v-for="(item, index) in document[1].items"
-            :key="`item-${index}`"
-            class="partner"
-          >
-            <prismic-rich-text :field="item.paragraph" />
-          </li>
-        </ul>
-      </div>
-    </section>
+    <section-slice
+      :content="document[1]"
+      :section-class="'partnerships'"
+      :ul-class="'partners'"
+      :li-class="'partner'"
+    ></section-slice>
 
-    <section class="diagnostic">
-      <div class="background"></div>
-      <div class="container padding-container">
-        <prismic-rich-text :field="document[2].primary.title" />
-        <prismic-rich-text :field="document[2].primary.description" />
-        <div class="features-wrapper">
-          <ul class="features">
-            <li
-              v-for="(item, index) in document[2].items"
-              :key="`item-${index}`"
-              class="feature"
-            >
-              <prismic-rich-text :field="item.paragraph" />
-            </li>
-          </ul>
-        </div>
-      </div>
-    </section>
+    <section-slice
+      :content="document[2]"
+      :section-class="'diagnostic'"
+      :ul-class="'features'"
+      :li-class="'feature'"
+    >
+    </section-slice>
 
-    <section class="certification">
-      <div class="background"></div>
-      <div class="container flex padding-container">
-        <div class="container-column">
-          <prismic-rich-text :field="document[3].primary.title" />
-          <prismic-rich-text :field="document[3].primary.description" />
-          <ul class="features">
-            <li
-              v-for="(item, index) in document[3].items"
-              :key="`item-${index}`"
-              class="feature"
-            >
-              <prismic-rich-text :field="item.paragraph" />
-            </li>
-          </ul>
-        </div>
-        <div class="container-column">
-          <div class="certification-illustration"></div>
-        </div>
-      </div>
-    </section>
+    <section-column-slice
+      :content="document[3]"
+      :section-class="'certification'"
+      :ul-class="'features'"
+      :li-class="'feature'"
+      :right-class="'certification-illustration'"
+    >
+    </section-column-slice>
 
-    <section class="prescription">
-      <div class="background"></div>
-      <div class="container padding-container">
-        <prismic-rich-text :field="document[4].primary.title" />
-        <prismic-rich-text :field="document[4].primary.description" />
-        <div class="features-wrapper">
-          <ul class="features">
-            <li
-              v-for="(item, index) in document[4].items"
-              :key="`item-${index}`"
-              class="feature"
-            >
-              <prismic-rich-text :field="item.paragraph" />
-            </li>
-          </ul>
-        </div>
-      </div>
-    </section>
+    <section-slice
+      :content="document[4]"
+      :section-class="'prescription'"
+      :ul-class="'features'"
+      :li-class="'feature'"
+    >
+    </section-slice>
 
     <section class="abc-pix">
       <div class="container padding-container">
         <div class="trapeze">
-          <!--          <img src="{{rootURL}}/images/ABCPix.svg" alt="" />-->
           <prismic-rich-text :field="document[5].primary.title" />
           <prismic-rich-text :field="document[5].primary.description" />
 
@@ -110,13 +65,17 @@
 
 <script>
 import DocumentFetcher from '~/services/document-fetcher'
+import SectionSlice from '~/components/slices/section'
+import SectionColumnSlice from '~/components/slices/section-column'
 
 export default {
+  components: {
+    SectionSlice,
+    SectionColumnSlice
+  },
   async asyncData({ app, error }) {
     try {
       const document = await DocumentFetcher(app.i18n).getMediation()
-      console.log('paragraph[0]', document.data.body[1].items[0].paragraph[0])
-      console.log('items', JSON.stringify(document.data.body[1].items))
       return {
         document: document.data.body
       }
