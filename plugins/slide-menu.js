50fdfd35c300035d8d87cb9a6bbc96cebddf2d15
Guillaume Lagorce <glagorce@octo.com>
2019-09-23 12:09:49 +0200

    Add vue-burger-menu component to handle burger menu

---
+++ plugins/slide-menu.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import {
+  Push,
+  PushRotate,
+  Reveal,
+  ScaleDown,
+  ScaleRotate,
+  Slide
+} from 'vue-burger-menu'
+
+Vue.component('SlideMenu', Slide)
+Vue.component('ScaleDownMenu', ScaleDown)
+Vue.component('ScaleRotateMenu', ScaleRotate)
+Vue.component('RevealMenu', Reveal)
+Vue.component('PushMenu', Push)
+Vue.component('PushRotateMenu', PushRotate)
