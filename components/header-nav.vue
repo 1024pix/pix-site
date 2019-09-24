565f4f137e07996bc81d663639b3524234c12c16
Guillaume Lagorce <glagorce@octo.com>
2019-09-24 16:03:55 +0200

    Use store and create dedicated header nav

---
+++ components/header-nav.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="nav">
+    <header>
+      <client-only>
+        <push-menu :width="'230'">
+          <burger-menu-nav :top-items="topItems" :bottom-items="bottomItems" />
+        </push-menu>
+      </client-only>
+      <div id="page-wrap">
+        <organization-nav :organization-nav-items="organizationNavItems" />
+        <div class="nav-principal desktop">
+          <div class="container padding-container">
+            <nuxt-link to="/">
+              <img
+                alt="Accueil du site pix.fr"
+                class="logo"
+                src="/images/pix-logo.svg"
+              />
+            </nuxt-link>
+            <img alt="Logo de la Marianne" src="/images/marianne-logo.svg" />
+            <div class="desktop">
+              <main-nav :main-nav-items="mainNavItems" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import MainNav from '~/components/main-nav'
+import BurgerMenuNav from '~/components/burger-menu-nav'
+import OrganizationNav from '~/components/organization-nav'
+
+export default {
+  name: 'HeaderNav',
+  computed: mapState([
+    'organizationNavItems',
+    'mainNavItems',
+    'topItems',
+    'bottomItems'
+  ]),
+  components: {
+    MainNav,
+    BurgerMenuNav,
+    OrganizationNav
+  }
+}
+</script>
+
+<style scoped></style>
---
+++ store/index.js
@@ -0,0 +1,22 @@
+import DocumentFetcher from '~/services/document-fetcher'
+
+export const state = () => ({
+  organizationNavItems: [],
+  mainNavItems: [],
+  topItems: [],
+  bottomItems: []
+})
+export const actions = {
+  async nuxtServerInit({ state }, { app }) {
+    function navItems(response, type) {
+      return response.data.body.filter((body) => body.primary.type === type)
+    }
+
+    const response = await DocumentFetcher(app.i18n).getNavigation()
+    state.organizationNavItems = navItems(response, 'organizations-nav')
+
+    state.mainNavItems = navItems(response, 'main-nav')
+    state.topItems = navItems(response, 'burger-menu-top')
+    state.bottomItems = navItems(response, 'burger-menu-bottom')
+  }
+}
