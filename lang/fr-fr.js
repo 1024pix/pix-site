178a70aa937492e2df54af8befa7d3adae4a208a
Guillaume Lagorce <glagorce@octo.com>
2019-09-24 09:26:41 +0200

    Suppress prettier errors

--- lang/fr-fr.js
+++ lang/fr-fr.js
@@ -1,9 +1,9 @@
 export default {
   'news-page-title': 'Actualités',
   'news-page-no-news': "Il n’y a pas encore d'actualités",
-  'announcement': 'Annonce',
-  'engineering': 'Ingénierie',
-  'event': 'Événement',
-  'feature': 'Nouveauté',
-  'society': 'Société'
+  announcement: 'Annonce',
+  engineering: 'Ingénierie',
+  event: 'Événement',
+  feature: 'Nouveauté',
+  society: 'Société'
 }
