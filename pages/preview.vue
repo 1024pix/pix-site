f999f9757914b2a1ea57d72c92e390bcc7c1d109
Vincent Hardouin <vincent.hardouin@pix.fr>
2019-09-26 21:44:38 +0200

    Add prismic preview

--- pages/preview.vue
+++ pages/preview.vue
@@ -1,13 +1,17 @@
 <template>
-  $END$
+  <div class="outer-container">
+    <p>Loading the Prismic's Preview...</p>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: "preview"
+import DocumentFetcher from '~/services/document-fetcher'
+export default {
+  name: 'Preview',
+  async asyncData({ query, redirect }) {
+    const url = await DocumentFetcher().getPreviewUrl(query.token)
+    redirect(url)
   }
+}
 </script>
-
-<style scoped>
-
-</style>
+<style scoped></style>
