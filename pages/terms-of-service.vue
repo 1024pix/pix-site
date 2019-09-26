de5df8238bf6afbc1c4a182e543ff5fa1ac513e5
Guillaume Lagorce <glagorce@octo.com>
2019-09-26 12:29:56 +0200

    Add terms of service page

---
+++ pages/terms-of-service.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="page terms-of-service">
+    <header class="page-header">
+      <div class="container md padding-container">
+        <h1 class="page-header__title">
+          <prismic-rich-text :field="document.title" />
+        </h1>
+      </div>
+    </header>
+
+    <div class="page-body">
+      <section class="page-section section-schools">
+        <div class="container md padding-container">
+          <div class="page-section__description">
+            <prismic-rich-text :field="document.body" />
+          </div>
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+import DocumentFetcher from '~/services/document-fetcher'
+
+export default {
+  name: 'TermsOfService',
+  nuxtI18n: {
+    paths: {
+      'fr-fr': '/conditions-generales-d-utilisation',
+      'en-gb': '/terms-of-service'
+    }
+  },
+  async asyncData({ app, error }) {
+    try {
+      const document = await DocumentFetcher(app.i18n).getTermsOfService()
+      return {
+        document: document.data
+      }
+    } catch (e) {
+      error({ statusCode: 404, message: 'Page not found' })
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.page-section__description {
+  h2 {
+    font-weight: 400;
+  }
+
+  ul {
+    list-style: none;
+  }
+}
+</style>
