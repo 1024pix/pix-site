d652a9aa6f8476315b546acb38d7c21669240bdc
Vincent Hardouin <vincent.hardouin@pix.fr>
2019-09-26 17:16:49 +0200

    Fix language dropdown

--- components/language-dropdown.vue
+++ components/language-dropdown.vue
@@ -11,43 +11,41 @@
 
 <script>
 import DropdownButton from '~/components/dropdown-button'
+
 export default {
   name: 'LanguageDropdown',
   components: {
     DropdownButton
-  },
-  computed: {
-    options() {
-      const availableLocale = [
-        { name: 'Français', lang: 'fr-fr' },
-        { name: 'English', lang: 'en-gb' }
-      ]
-      const currentLocale = this.$i18n.locale || this.$i18n.defaultLocale
-
-      const selectableLocale = availableLocale.filter(
-        (locale) => locale.lang !== currentLocale
-      )
-      return selectableLocale
-    }
   },
   data() {
     const availableLocale = [
       { name: 'Français', lang: 'fr-fr' },
       { name: 'English', lang: 'en-gb' }
     ]
-    const currentLocale = this.$i18n.locale || this.$i18n.defaultLocale
-
-    const currentLanguage = availableLocale.find(
-      (locale) => locale.lang === currentLocale
-    )
     return {
-      currentLanguage
+      availableLocale
+    }
+  },
+  computed: {
+    currentLocale() {
+      return this.$i18n.locale || this.$i18n.defaultLocale
+    },
+    currentLanguage() {
+      return this.availableLocale.find(
+        (locale) => locale.lang === this.currentLocale
+      )
+    },
+    options() {
+      return this.availableLocale.filter(
+        (locale) => locale.lang !== this.currentLocale
+      )
     }
   },
 
   methods: {
     languageDidChange(selectedLocale) {
-      const page = this.switchLocalePath(selectedLocale.lang)
+      this.$i18n.setLocale(selectedLocale.lang)
+      const page = this.switchLocalePath()
       this.$router.push(page)
     }
   }
