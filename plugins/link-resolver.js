ae7828ad8674aebba8db8c56712c277a8543196f
Vincent Hardouin <vincent.hardouin@pix.fr>
2019-09-27 17:07:42 +0200

    Refacto link-resolver

--- plugins/link-resolver.js
+++ plugins/link-resolver.js
@@ -1,36 +1,23 @@
 export default function(doc) {
-  switch (doc.type) {
-    case 'index': {
-      return doc.lang === 'fr-fr' ? `/` : `${doc.lang}/`
-    }
-    case 'mediation': {
-      return doc.lang === 'fr-fr' ? `/${doc.uid}` : `${doc.lang}/${doc.uid}`
-    }
-    case 'employers': {
-      return doc.lang === 'fr-fr' ? `/${doc.uid}` : `${doc.lang}/${doc.uid}`
-    }
-    case 'about': {
-      return doc.lang === 'fr-fr' ? `/${doc.uid}` : `${doc.lang}/${doc.uid}`
-    }
-    case 'school-education': {
-      return doc.lang === 'fr-fr' ? `/${doc.uid}` : `${doc.lang}/${doc.uid}`
-    }
-    case 'higher-education': {
-      return doc.lang === 'fr-fr' ? `/${doc.uid}` : `${doc.lang}/${doc.uid}`
-    }
-    case 'competences': {
-      return doc.lang === 'fr-fr' ? `/${doc.uid}` : `${doc.lang}/${doc.uid}`
-    }
-    case 'stats': {
-      return doc.lang === 'fr-fr' ? `/${doc.uid}` : `${doc.lang}/${doc.uid}`
-    }
-    case 'legal-notices': {
-      return doc.lang === 'fr-fr' ? `/${doc.uid}` : `${doc.lang}/${doc.uid}`
-    }
-    case 'news_item': {
-      return doc.lang === 'fr-fr'
-        ? `/actualites/${doc.uid}`
-        : `${doc.lang}/news/${doc.uid}`
-    }
+  const staticRoute = [
+    'mediation',
+    'employers',
+    'about',
+    'school-education',
+    'higher-education',
+    'competences',
+    'stats',
+    'legal-notices'
+  ]
+  if (staticRoute.includes(doc.type)) {
+    return doc.lang === 'fr-fr' ? `/${doc.uid}` : `${doc.lang}/${doc.uid}`
+  }
+  if (doc.type === 'news_item') {
+    return doc.lang === 'fr-fr'
+      ? `/actualites/${doc.uid}`
+      : `${doc.lang}/news/${doc.uid}`
+  }
+  if (doc.type === 'index') {
+    return doc.lang === 'fr-fr' ? `/` : `${doc.lang}/`
   }
 }
