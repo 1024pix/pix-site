16464302bd9093fb1d20804a7a2f510dd9788833
Guillaume Lagorce <glagorce@octo.com>
2019-09-30 11:39:24 +0200

    Set fr-be as defaultLanguage

--- plugins/link-resolver.js
+++ plugins/link-resolver.js
@@ -1,3 +1,5 @@
+const defaultLocale = 'fr-be'
+
 export default function(doc) {
   const staticRoute = [
     'mediation',
@@ -10,14 +12,14 @@ export default function(doc) {
     'legal-notices'
   ]
   if (staticRoute.includes(doc.type)) {
-    return doc.lang === 'fr-fr' ? `/${doc.uid}` : `${doc.lang}/${doc.uid}`
+    return doc.lang === defaultLocale ? `/${doc.uid}` : `${doc.lang}/${doc.uid}`
   }
   if (doc.type === 'news_item') {
-    return doc.lang === 'fr-fr'
+    return doc.lang === defaultLocale
       ? `/actualites/${doc.uid}`
       : `${doc.lang}/news/${doc.uid}`
   }
   if (doc.type === 'index') {
-    return doc.lang === 'fr-fr' ? `/` : `${doc.lang}/`
+    return doc.lang === defaultLocale ? `/` : `${doc.lang}/`
   }
 }
