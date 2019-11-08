7a86430b9341353eea87168c4db9ace64ab02200
Vincent Hardouin <vincent.hardouin@pix.fr>
2019-11-08 16:31:12 +0100

    Add help page who redirect to support.pix.fr

---
+++ pages/help.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  middleware: 'redirect-to-external-link',
+  name: 'Help',
+  nuxtI18n: {
+    paths: {
+      'fr-fr': '/aide',
+      'en-gb': '/help'
+    }
+  }
+}
+</script>
