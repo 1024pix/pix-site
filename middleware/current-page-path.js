8b5377b0afe50a9bf2de32b15cd20e2a222cea27
Guillaume Lagorce <guillaume@hey-gul.com>
2020-05-14 07:02:25 +0200

    Adapt relative links and routing for '.org' and '.fr'

--- middleware/current-page-path.js
+++ middleware/current-page-path.js
@@ -1,5 +1,18 @@
 export default function(context) {
   const { req, route } = context
-  const host = req ? req.headers.host : window.location.host
+  const host = _getHostFromRequest(req)
+    ? _getHostFromRequest(req)
+    : window.location.host
+
   context.currentPagePath = `${host}${route.path}`
+}
+
+function _getHostFromRequest(req) {
+  if (!req) {
+    return
+  }
+  const headers = req.headers
+  return headers['x-forwarded-host']
+    ? headers['x-forwarded-host']
+    : headers.host
 }
