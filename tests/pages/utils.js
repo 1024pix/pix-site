68572f29239848999c87343425e939fc768332b7
Antoine Ceol <antoine.ceol@gmail.com>
2020-07-24 19:15:33 +0200

    Enable vue pages tests

---
+++ tests/pages/utils.js
@@ -0,0 +1,34 @@
+import { shallowMount } from '@vue/test-utils'
+
+/**
+ * This is needed to manage the asyncData() from vuepages
+ * @param {*} page the filename of the page without extension
+ */
+export async function getInitialised(page, opts) {
+  const Page = require('@/pages/' + page).default
+  if (!Page.asyncData) {
+    return shallowMount(Page, opts)
+  }
+
+  let originalData = {}
+  if (Page.data != null) {
+    originalData = Page.data()
+  }
+  const asyncData = await Page.asyncData({
+    app: {
+      i18n: 'fr-fr',
+    },
+    error: jest.fn(),
+    req: {},
+    currentPagePath: '',
+  })
+
+  Page.data = function () {
+    return {
+      ...originalData,
+      ...asyncData,
+    }
+  }
+
+  return shallowMount(Page, opts)
+}
