8adf3f8b5ee7a27f4b1c218f0285c66da1248f5d
D0C702WH0 <jemyplu@gmail.com>
2020-07-31 14:20:49 +0200

    BSR BEM scss

--- pages/verify-certification-score.vue
+++ pages/verify-certification-score.vue
@@ -1,36 +1,46 @@
 <template>
   <div class="page competences">
     <main class="page-body">
-      <div class="container lg">
-        <h1 class="title">{{ title }}</h1>
-        <form novalidate @submit="submit">
-          <div class="input-field">
+      <div class="container lg certificate-verification-page">
+        <h1 class="certificate-verification-page__title">{{ title }}</h1>
+        <form
+          class="certificate-verification-page__form"
+          novalidate
+          @submit="submit"
+        >
+          <div class="certificate-verification-page__input-field">
             <label for="code">{{ verificationCodeLabel }}</label>
             <input
               id="code"
               v-model="code"
-              :class="{ 'regex-error': hasCodeRegexError }"
+              :class="[hasCodeRegexError ? 'regex-error' : 'no-regex-error']"
               type="text"
               required="true"
               :pattern="codeRegex"
               name="verificationCode"
             />
-            <p v-if="hasCodeRegexError" class="error-paragraph">
+            <p
+              v-if="hasCodeRegexError"
+              class="certificate-verification-page__error-for-input"
+            >
               Votre code de vérification ne correspond pas au format: P-XXXXX
             </p>
           </div>
-          <div class="input-field">
+          <div class="certificate-verification-page__input-field">
             <label for="score">{{ candidateScoreLabel }}</label>
             <input
               id="score"
               v-model="score"
-              :class="{ 'regex-error': hasScoreRegexError }"
+              :class="[hasScoreRegexError ? 'regex-error' : 'no-regex-error']"
               type="text"
               required="true"
               :pattern="scoreRegex"
               name="candidateScore"
             />
-            <p v-if="hasScoreRegexError" class="error-paragraph">
+            <p
+              v-if="hasScoreRegexError"
+              class="certificate-verification-page__error-for-input"
+            >
               Le score du candidat doit être composé de 3 chiffres
             </p>
           </div>
@@ -38,7 +48,7 @@
             {{ buttonLabel }}
           </button>
         </form>
-        <div v-if="formSent" class="form-sent">
+        <div v-if="formSent" class="certificate-verification-page__form-sent">
           <fa :icon="fas.faExclamationTriangle" />
           <span>Il n'y a pas de certificat Pix correspondant</span>
         </div>
@@ -141,75 +151,91 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.title {
-  font-family: 'Open Sans', Arial, sans-serif;
-  font-size: 62px;
-  height: 164px;
-  line-height: 82px;
-  font-weight: 300;
-  margin-bottom: 50px;
-  max-width: 490px;
-}
 .page-body {
-  form {
-    width: 50%;
-    display: flex;
-    flex-direction: column;
-    label {
-      font-family: 'Roboto', Arial, sans-serif;
-      color: $blue-5;
+  .certificate-verification-page {
+    &__title {
+      font-family: 'Open Sans', Arial, sans-serif;
+      font-size: 62px;
+      height: 164px;
+      line-height: 82px;
+      font-weight: 300;
+      margin-bottom: 50px;
+      max-width: 490px;
     }
-    .input-field {
+
+    &__form {
+      width: 50%;
+      display: flex;
+      flex-direction: column;
+
+      button {
+        width: 175px;
+        max-height: 36px;
+        line-height: 100%;
+      }
+    }
+
+    &__input-field {
       margin-bottom: 24px;
       display: flex;
       flex-direction: column;
+
+      label {
+        font-family: 'Roboto', Arial, sans-serif;
+        color: $blue-5;
+      }
+
+      .no-regex-error {
+        border: 2px solid $grey-9;
+      }
+
+      .regex-error {
+        border: 2px solid $error;
+      }
+
       input {
         padding: 0 8px;
         font-size: 1rem;
-        border: 2px solid $grey-9;
         border-radius: 4px;
         max-width: 475px;
         height: 41px;
       }
-      .regex-error {
-        border: 2px solid $error;
-      }
-      .error-paragraph {
-        color: $error;
-        margin: 0;
-        font-size: 14px;
-      }
     }
-    button {
-      width: 175px;
-      max-height: 36px;
-      line-height: 100%;
-    }
-  }
-  .form-sent {
-    margin-top: 32px;
-    background-color: $error-2;
-    border-radius: 4px;
-    height: 46px;
-    line-height: 46px;
-    width: 436px;
-    text-align: center;
-    svg {
+
+    &__error-for-input {
       color: $error;
-      margin-right: 6px;
-    }
-    span {
-      color: $error;
-      font-family: 'Roboto', Arial, sans-serif;
+      margin: 0;
       font-size: 14px;
-      font-weight: normal;
-      height: 22px;
-      letter-spacing: 0.15px;
-      width: 380px;
     }
-  }
-  .error {
-    color: $error;
+
+    &__form-sent {
+      margin-top: 32px;
+      background-color: $error-2;
+      border-radius: 4px;
+      height: 46px;
+      line-height: 46px;
+      width: 436px;
+      text-align: center;
+
+      svg {
+        color: $error;
+        margin-right: 6px;
+      }
+
+      span {
+        color: $error;
+        font-family: 'Roboto', Arial, sans-serif;
+        font-size: 14px;
+        font-weight: normal;
+        height: 22px;
+        letter-spacing: 0.15px;
+        width: 380px;
+      }
+    }
+
+    .error {
+      color: $error;
+    }
   }
 }
 </style>
