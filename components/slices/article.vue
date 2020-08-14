6641acf8aa73d190bad67f7a25b1ce274fd49e54
Alexandre COIN <alexandre.pc.coin@gmail.com>
2020-08-14 17:06:38 +0200

    Add article component

---
+++ components/slices/article.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <prismic-rich-text :field="title" class="title--big" />
+    <div v-for="item in content" :key="item" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Article',
+  props: {
+    content: {
+      type: Object,
+      default: null,
+    },
+  },
+  computed: {
+    title() {
+      return this.content.primary['article-title']
+    },
+  },
+}
+</script>
+
+<style></style>
