7eea1acdbb42873158212573a65676d3496347cf
Vincent Hardouin <vincent.hardouin@pix.fr>
2020-08-17 19:22:27 +0200

    Add default style for section slice

--- components/slices/section.vue
+++ components/slices/section.vue
@@ -33,23 +33,23 @@ export default {
     },
     sectionClass: {
       type: String,
-      default: '',
+      default: 'section',
     },
     containerClass: {
       type: String,
-      default: '',
+      default: 'section__container',
     },
     flexContainerClass: {
       type: String,
-      default: '',
+      default: 'section__flex-container',
     },
     flexContentClass: {
       type: String,
-      default: '',
+      default: 'section__flex-content',
     },
     buttonClass: {
       type: String,
-      default: '',
+      default: 'section__button',
     },
   },
   computed: {
@@ -81,4 +81,65 @@ export default {
 }
 </script>
 
-<style></style>
+<style lang="scss">
+.section {
+  font-family: $open-sans, Arial, sans-serif;
+  text-align: center;
+  margin: 0 auto;
+  padding: 32px 0;
+  @include device-is('large-mobile') {
+    padding: 64px 0;
+  }
+
+  &__container {
+    max-width: 1140px;
+    margin: 0 auto;
+  }
+
+  &__flex-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: column;
+
+    @include device-is('large-mobile') {
+      flex-direction: row;
+    }
+  }
+
+  &__flex-content {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    max-width: 80%;
+
+    @include device-is('large-mobile') {
+      max-width: 45%;
+    }
+
+    h2 {
+      color: $blue-5;
+      font-size: 2rem;
+      font-family: $open-sans;
+      font-weight: normal;
+      letter-spacing: 0.00875rem;
+      line-height: 2.875rem;
+      text-align: left;
+    }
+
+    p {
+      color: $grey-10;
+      font-size: 1.25rem;
+      line-height: 2rem;
+      text-align: justify;
+
+      &.block-img img {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        max-width: 100%;
+      }
+    }
+  }
+}
+</style>
