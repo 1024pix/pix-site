ba30a8a9c9e5178e1484dadf466d685176ec472a
Alexandre COIN <alexandre.pc.coin@gmail.com>
2020-08-17 15:17:30 +0200

    Set article component's architecture

--- components/slices/article.vue
+++ components/slices/article.vue
@@ -1,7 +1,13 @@
 <template>
-  <div>
-    <prismic-rich-text :field="title" class="title--big" />
-    <div v-for="item in content" :key="item" />
+  <div class="article">
+    <div class="article__text">
+      <prismic-rich-text :field="title" class="title--big" />
+      <prismic-rich-text :field="description" />
+      <a :href="buttonLink" target="_blank">{{ buttonText }}</a>
+    </div>
+    <div class="article__image">
+      <img :src="image" />
+    </div>
   </div>
 </template>
 
@@ -15,11 +21,35 @@ export default {
     },
   },
   computed: {
+    buttonLink() {
+      return this.content.primary['article-link-url'].url
+    },
+    buttonText() {
+      return this.content.primary['article-link-text']
+    },
+    description() {
+      return this.content.primary['article-description']
+    },
+    image() {
+      return this.content.primary['article-image'].url
+    },
     title() {
+      console.log(this.content.primary)
       return this.content.primary['article-title']
     },
   },
 }
 </script>
 
-<style></style>
+<style>
+.article {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+}
+
+.article__text {
+  display: flex;
+  flex-direction: column;
+}
+</style>
