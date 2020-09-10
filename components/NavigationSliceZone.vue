1dd955abd6214bf8e9e29d39e5f717e8df78da59
celineung <celine.ung@zenika.com>
2020-09-10 17:14:28 +0200

    Add navigation-zone to mainNavigation

--- components/NavigationSliceZone.vue
+++ components/NavigationSliceZone.vue
@@ -13,6 +13,9 @@
       <template v-if="slice.slice_type === 'logos_zone'">
         <logos-zone :slice="slice" />
       </template>
+      <template v-if="slice.slice_type === 'navigation_zone'">
+        <navigation-zone :slice="slice" />
+      </template>
     </section>
   </div>
 </template>
@@ -20,16 +23,28 @@
 <script>
 import { mapState } from 'vuex'
 import LogosZone from '@/components/slices/LogosZone'
+import NavigationZone from '@/components/slices/NavigationZone'
 import BurgerMenuNav from '@/components/BurgerMenuNav'
 
 export default {
   name: 'NavigationSliceZone',
   components: {
     LogosZone,
+    NavigationZone,
     BurgerMenuNav,
   },
   computed: mapState(['mainNavigation']),
 }
 </script>
 
-<style></style>
+<style lang="scss">
+.navigation-slice-zone {
+  display: flex;
+  justify-content: flex-end;
+
+  @include device-is('large-screen') {
+    justify-content: center;
+    margin: 0 98px;
+  }
+}
+</style>
---
+++ components/slices/NavigationZone.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="navigation-zone">
+    <div v-for="(menuItem, index) in slice.items" :key="`item-${index}`">
+      <pix-link :field="menuItem.link" class="navigation-zone__item">
+        {{ $prismic.asText(menuItem.name) }}
+      </pix-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavigationZone',
+  props: {
+    slice: {
+      type: Object,
+      default: null,
+    },
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.navigation-zone {
+  display: none;
+  justify-content: space-around;
+
+  @include device-is('large-screen') {
+    display: flex;
+    width: 652px;
+    align-items: center;
+    height: 100%;
+
+    &__item {
+      color: $grey-9;
+      font-family: $font-roboto;
+      font-size: 14px;
+      font-weight: 500;
+      height: 22px;
+      letter-spacing: 0.13px;
+      line-height: 22px;
+    }
+
+    a {
+      &:active,
+      &:hover {
+        color: $blue-1;
+      }
+    }
+  }
+}
+</style>
