64069fbac0bcd50adb650733695e9a33eea761f1
melanieb <melanie.boudard@gmail.com>
2020-09-25 15:56:47 +0200

    Add component PixImage

---
+++ components/PixImage.vue
@@ -0,0 +1,26 @@
+<template>
+  <prismic-image :field="image" :role="image.role" />
+</template>
+
+<script>
+export default {
+  name: 'PixImage',
+  props: {
+    field: {
+      required: true,
+      type: Object,
+      default: null,
+    },
+  },
+  computed: {
+    image() {
+      const image = Object.assign({}, this.field)
+      if (!image.alt) {
+        image.alt = ''
+        image.role = 'presentation'
+      }
+      return image
+    },
+  },
+}
+</script>
