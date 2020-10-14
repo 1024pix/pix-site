a143f4d64a3c9cfde0da3894ebe86a4e41f21ea6
celineung <celine.ung@zenika.com>
2020-10-14 10:33:37 +0200

    Add footer in vuex store

--- store/index.js
+++ store/index.js
@@ -5,6 +5,7 @@ export const state = () => ({
   hotNews: null,
   host: null,
   mainNavigations: [],
+  mainFooters: [],
 })
 export const actions = {
   async nuxtServerInit({ commit }, { app, req }) {
@@ -13,6 +14,7 @@ export const actions = {
       'updateMainNavigations',
       await getMainNavigations(app.$prismic, app.i18n)
     )
+    commit('updateMainFooters', await getMainFooters(app.$prismic, app.i18n))
     commit('updateHotNews', await getHotNews(app.$prismic, app.i18n))
     commit('updateHost', req)
   },
@@ -21,6 +23,9 @@ export const actions = {
   },
   async updateMainNavigations({ commit }, { i18n, prismic }) {
     commit('updateMainNavigations', await getMainNavigations(prismic, i18n))
+  },
+  async updateMainFooters({ commit }, { i18n, prismic }) {
+    commit('updateMainFooters', await getMainFooters(prismic, i18n))
   },
 }
 export const mutations = {
@@ -49,6 +54,9 @@ export const mutations = {
   updateMainNavigations(state, navigations) {
     state.mainNavigations = [...navigations]
   },
+  updateMainFooters(state, footers) {
+    state.mainFooters = [...footers]
+  },
   updateHotNews(state, hotNews) {
     state.hotNews = hotNews && hotNews.data ? hotNews.data.description : null
   },
@@ -60,6 +68,10 @@ function getNavigation(prismic, i18n) {
 
 function getMainNavigations(prismic, i18n) {
   return documentFetcher(prismic, i18n).findByType(documents.mainNavigation)
+}
+
+function getMainFooters(prismic, i18n) {
+  return documentFetcher(prismic, i18n).findByType(documents.mainFooter)
 }
 
 function getHotNews(prismic, i18n) {
