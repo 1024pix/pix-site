be82c4b44aa31e732a9e93e5e59c3660ee2be383
melanieb <melanie.boudard@gmail.com>
2020-10-15 14:05:22 +0200

    Force redirection to /employeurs because there is no / page

--- middleware/pix-pro/current-page-path.js
+++ middleware/pix-pro/current-page-path.js
@@ -1,5 +1,9 @@
 export default function (context) {
-  const { req, route } = context
+  const { req, route, redirect } = context
   const host = req ? req.headers.host : window.location.host
+
+  if (req && req.url === '/') {
+    redirect(`${host}/employeurs`)
+  }
   context.currentPagePath = `${host}${route.path}`
 }
