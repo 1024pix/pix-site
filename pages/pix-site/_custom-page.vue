ea8807b3ac0f4e898e21454edda10d0bdd162925
Stephane Bedeau <sbedeau@octo.com>
2020-10-16 22:00:09 +0200

    Add latest news items to custom page

--- pages/pix-site/_custom-page.vue
+++ pages/pix-site/_custom-page.vue
@@ -4,7 +4,7 @@
       <simple-page :content="document.data" />
     </div>
     <div v-if="type === 'slices_page'">
-      <slice-zone :slices="document.data.body" />
+      <slice-zone :slices="slices" />
     </div>
   </div>
 </template>
@@ -34,13 +34,30 @@ export default {
         app.i18n,
         req
       ).getPageByUid(params.uid)
+
       const meta = document.data.meta
-      return { currentPagePath, meta, document }
+
+      const latestNewsItems = await documentFetcher(
+        app.$prismic,
+        app.i18n,
+        req
+      ).findNewsItems({ page: 1, pageSize: 3 })
+
+      return { currentPagePath, meta, document, latestNewsItems }
     } catch (e) {
       error({ statusCode: 404, message: 'Page not found' })
     }
   },
   computed: {
+    slices() {
+      const rawDocumentSlices = this.document.data.body
+      return rawDocumentSlices.map((slice, index) => {
+        if (slice.slice_type === 'latest_news') {
+          slice.primary.latest_news_items = this.latestNewsItems
+        }
+        return slice
+      })
+    },
     type() {
       return this.document.type
     },
