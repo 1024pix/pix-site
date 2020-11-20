a1a2175944476ad2871a22a3b08289666a025af6
melanieb <melanie.boudard@gmail.com>
2020-11-20 16:42:44 +0100

    Add ChartSection in Stat Slice

--- components/slices/Stat.vue
+++ components/slices/Stat.vue
@@ -2,26 +2,59 @@
   <div class="stat">
     <prismic-rich-text :field="content.block_title" />
     <prismic-rich-text :field="content.block_presentation" />
+    <chart-section :data="chartData" />
   </div>
 </template>
 
 <script>
+import ChartSection from '~/components/ChartSection'
+
 export default {
   name: 'StatSlice',
+  components: {
+    ChartSection,
+  },
   props: {
     slice: {
       type: Object,
       default: null,
     },
   },
-  data() {
-    return {
-      rows: [],
-    }
-  },
   computed: {
     content() {
       return this.slice.primary
+    },
+    data() {
+      return this.slice.items
+    },
+
+    chartData() {
+      function backgroundColorFrom(color) {
+        const hex = color.replace('#', '')
+        const r = parseInt(hex.substring(0, 2), 16)
+        const g = parseInt(hex.substring(2, 4), 16)
+        const b = parseInt(hex.substring(4, 6), 16)
+
+        return 'rgba(' + r + ',' + g + ',' + b + ',0.2)'
+      }
+
+      const valueData = this.data.map((data) => data.data_value)
+      const labelData = this.data.map((data) => data.data_date)
+      const chartData = {
+        datasets: [
+          {
+            backgroundColor: backgroundColorFrom(
+              this.content.block_graph_color
+            ),
+            borderColor: this.content.block_graph_color,
+            data: valueData,
+            label: this.content.block_data_name[0].text,
+            type: 'line',
+          },
+        ],
+        labels: labelData,
+      }
+      return chartData
     },
   },
 }
@@ -29,10 +62,16 @@ export default {
 
 <style lang="scss">
 .stat {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 0 auto;
+  text-align: center;
   max-width: 1140px;
+  margin: 0 50px;
+
+  @include device-is('desktop') {
+    margin: 0 auto;
+  }
+
+  canvas {
+    margin: 40px 0;
+  }
 }
 </style>
