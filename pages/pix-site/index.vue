e0dbc3afd9799ab95ccd8e759f2901d750d52d02
melanieb <melanie.boudard@gmail.com>
2020-11-27 16:17:56 +0100

    Update queried Prismic index page

--- pages/pix-site/index.vue
+++ pages/pix-site/index.vue
@@ -6,53 +6,65 @@
 
 <script>
 import SliceZone from '@/components/SliceZone'
-import { documents, documentFetcher } from '~/services/document-fetcher'
+import { documentFetcher } from '~/services/document-fetcher'
 
 export default {
+  name: 'Index',
+  nuxtI18n: {
+    paths: {
+      fr: '/',
+      'fr-fr': '/',
+      'en-gb': '/',
+    },
+  },
   components: {
     SliceZone,
   },
-  async asyncData({ app, error, req, currentPagePath }) {
+  async asyncData({ params, app, req, error, currentPagePath }) {
     try {
-      const document = await documentFetcher(app.$prismic, app.i18n, req).get(
-        documents.index
-      )
+      const document = await documentFetcher(
+        app.$prismic,
+        app.i18n,
+        req
+      ).getPageByUid('index-pix-site')
+
+      const meta = document.data.meta
 
       const latestNewsItems = await documentFetcher(
         app.$prismic,
         app.i18n,
         req
       ).findNewsItems({ page: 1, pageSize: 3 })
-
-      return {
-        currentPagePath,
-        meta: document.data.meta,
-        document: document.data.body,
-        latestNewsItems,
-      }
+      return { currentPagePath, meta, document, latestNewsItems }
     } catch (e) {
-      // eslint-disable-next-line no-console
-      console.error({ e })
       error({ statusCode: 404, message: 'Page not found' })
     }
   },
-  head() {
-    const meta = this.$getMeta(this.meta, this.currentPagePath, this.$prismic)
-    const pageTitle = this.$t('page-titles.index')
-    return {
-      title: pageTitle,
-      meta,
-    }
-  },
+
   computed: {
     slices() {
-      return this.document.map((slice, index) => {
+      const rawDocumentSlices = this.document.data.body
+      return rawDocumentSlices.map((slice, index) => {
         if (slice.slice_type === 'latest_news') {
           slice.primary.latest_news_items = this.latestNewsItems
         }
         return slice
       })
     },
+    type() {
+      return this.document.type
+    },
+    title() {
+      return this.document.data.title[0].text
+    },
+  },
+  head() {
+    const meta = this.$getMeta(this.meta, this.currentPagePath, this.$prismic)
+
+    return {
+      meta,
+      title: `${this.title} | Pix`,
+    }
   },
 }
 </script>
