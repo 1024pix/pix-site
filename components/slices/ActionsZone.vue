893d4dd7b1a6d09618794a501fe68eb1237de340
melanieb <melanie.boudard@gmail.com>
2021-04-09 14:54:57 +0200

    Set list of link in menu in a html list

--- components/slices/ActionsZone.vue
+++ components/slices/ActionsZone.vue
@@ -1,19 +1,20 @@
 <template>
   <nav class="actions-zone">
     <language-switcher type="with-dropdown" />
-
-    <div v-for="(menuItem, index) in slice.items" :key="`item-${index}`">
-      <pix-link
-        :field="menuItem.link"
-        class="actions-zone__item"
-        :class="{
-          button: menuItem.style === 'button',
-          link: menuItem.style === 'link',
-        }"
-      >
-        {{ $prismic.asText(menuItem.name) }}
-      </pix-link>
-    </div>
+    <ul>
+      <li v-for="(menuItem, index) in slice.items" :key="`item-${index}`">
+        <pix-link
+          :field="menuItem.link"
+          class="actions-zone__item"
+          :class="{
+            button: menuItem.style === 'button',
+            link: menuItem.style === 'link',
+          }"
+        >
+          {{ $prismic.asText(menuItem.name) }}
+        </pix-link>
+      </li>
+    </ul>
   </nav>
 </template>
 
@@ -40,6 +41,18 @@ export default {
   align-items: center;
   height: 100%;
   margin-left: 5px;
+
+  ul {
+    list-style: none;
+    display: flex;
+    li::before {
+      content: none;
+    }
+    li {
+      align-self: center;
+      display: inline;
+    }
+  }
 
   & > div:not(:last-child) {
     margin-right: 16px;
