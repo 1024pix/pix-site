b8929a93efabf62e4658a6fb8a2a66cf0d778858
Vincent Hardouin <vincent.hardouin@pix.fr>
2021-07-01 17:44:37 +0200

    fix: lint nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,8 +40,7 @@ const config = {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
         rel: 'stylesheet',
-        href:
-          'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400&family=Roboto:wght@400;500;600&display=swap',
+        href: 'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400&family=Roboto:wght@400;500;600&display=swap',
       },
     ],
   },
