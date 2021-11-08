8e904d5f83991b7e89eeca092565cee168ff8cac
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2021-11-08 11:05:38 +0100

    tests: fix nginx tests

--- tests.sh
+++ tests.sh
@@ -8,12 +8,13 @@ function checkRedirect() {
   local host=$1
   local path=$2
   local forwardedHost=$3
-  local expectedStatus=$4
-  local expectedLocation=$5
+  local forwardedScheme=$4
+  local expectedStatus=$5
+  local expectedLocation=$6
 
   echo -e "\nWhen url = $host$path and X-Forwarded-Host header = $forwardedHost"
   echo -e "   It returns $expectedStatus and location header = $expectedLocation"
-  local response=$(curl -w "%{http_code} %{redirect_url}" -k http://localhost$path -H "Host: $host" -H "X-Forwarded-Host: $forwardedHost" -o /dev/null --silent)
+  local response=$(curl -w "%{http_code} %{redirect_url}" -k http://localhost$path -H "Host: $host" -H "X-Forwarded-Host: $forwardedHost" -H "X-Forwarded-Proto: $forwardedScheme" -o /dev/null --silent)
 
   if [ "$response" != "$expectedStatus $expectedLocation" ]; then
     echo "${RED}❌ Expected $expectedStatus $expectedLocation for url $host$path, got $response ${RESET}"
@@ -23,17 +24,16 @@ function checkRedirect() {
   fi
 }
 
-checkRedirect pix.fr / "" 200
-checkRedirect pix.fr /competences "" 301 http://pix.fr/competences/
-checkRedirect pix.org /_nuxt/LICENSES "" 200
-checkRedirect pix.org /images/ "" 403
-checkRedirect pix.fr /aide "" 301 https://support.pix.org/
-checkRedirect pix.fr /help "" 301 https://support.pix.org/
-checkRedirect pix.fr /employeurs "" 301 https://pro.pix.fr/
-checkRedirect pix.org / "" 301 http://pix.org/fr/
-checkRedirect review.scalingo.io / "review.pix.org" 301 http://review.pix.org/fr/
-checkRedirect pix.org /_assets/ "" 403
-checkRedirect review.scalingo.io /_assets/ "review.pix.org" 403
-checkRedirect pix.org /favicon.ico "" 200
-checkRedirect pix.org /favicon.ico "review.pix.org" 200
-
+checkRedirect pix.fr / "" "" 200
+checkRedirect pix.fr /competences "" "" 301 http://pix.fr/competences/
+checkRedirect pix.org /_nuxt/LICENSES "" "" 200
+checkRedirect pix.org /images/ "" "" 403
+checkRedirect pix.fr /aide "" "" 301 https://support.pix.org/
+checkRedirect pix.fr /help "" "" 301 https://support.pix.org/
+checkRedirect pix.fr /employeurs "" "" 301 https://pro.pix.fr/
+checkRedirect pix.org / "" "" 301 http://pix.org/fr/
+checkRedirect review.scalingo.io / "review.pix.org" "https" 301 https://review.pix.org/fr/
+checkRedirect pix.org /_assets/ "" "" 403
+checkRedirect review.scalingo.io /_assets/ "review.pix.org" "https" 403
+checkRedirect pix.org /favicon.ico "" "" 200
+checkRedirect pix.org /favicon.ico "review.pix.org" "" 200
