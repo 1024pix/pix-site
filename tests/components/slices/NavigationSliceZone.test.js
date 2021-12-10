549759a12c05d56c1608579bacd4ccb7a4c5d52d
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2021-12-10 16:13:19 +0100

    refactor: remove useless tests
    NavigationSliceZone is now SITE agnostic

--- tests/components/slices/NavigationSliceZone.test.js
+++ tests/components/slices/NavigationSliceZone.test.js
@@ -36,99 +36,23 @@ describe('NavigationSliceZone', () => {
     },
   }
 
-  const expectedProNavigation = {
-    data: {
-      navigation_for: 'pix-pro',
-      body: [
-        {
-          slice_type: 'logos_zone',
-          items: [],
-        },
-        {
-          slice_type: 'navigation_zone',
-          items: [],
-        },
-        {
-          slice_type: 'actions_zone',
-          items: [],
-        },
-      ],
-    },
-  }
-
   describe('Slice: NavigationSliceZone', () => {
-    afterEach(() => {
-      process.env = {
-        isPixPro: false,
-      }
-    })
-
     describe('#usedMainNavigation', () => {
-      describe('When we are in pix-site and we have the site navigation', () => {
         it('should return the site navigation', () => {
-          // given
-          component = shallowMount(NavigationSliceZone, {
+            // given
+            component = shallowMount(NavigationSliceZone, {
             data() {
-              return { usedMainNavigation: expectedSiteNavigation.data.body }
+                return { usedMainNavigation: expectedSiteNavigation.data.body }
             },
             stubs,
-          })
+            })
 
-          // when
-          const result = component.vm.usedMainNavigation
+            // when
+            const result = component.vm.usedMainNavigation
 
-          // then
-          expect(result).toEqual(expectedSiteNavigation.data.body)
+            // then
+            expect(result).toEqual(expectedSiteNavigation.data.body)
         })
-      })
-
-      describe('When we are in pix-pro and we have the pro navigation', () => {
-        beforeEach(() => {
-          process.env = {
-            isPixPro: true,
-          }
-        })
-
-        it('should return the pro navigation', () => {
-          // given
-          component = shallowMount(NavigationSliceZone, {
-            data() {
-              return { usedMainNavigation: expectedProNavigation.data.body }
-            },
-            stubs,
-          })
-
-          // when
-          const result = component.vm.usedMainNavigation
-
-          // then
-          expect(result).toEqual(expectedProNavigation.data.body)
-        })
-      })
-
-      describe('When we are in pix-pro and we have only the site navigation', () => {
-        beforeEach(() => {
-          process.env = {
-            isPixPro: true,
-          }
-        })
-
-        it('should return the site navigation', () => {
-          // given
-          component = shallowMount(NavigationSliceZone, {
-            data() {
-              return { usedMainNavigation: expectedProNavigation.data.body }
-            },
-            stubs,
-          })
-
-          // when
-          const result = component.vm.usedMainNavigation
-
-          // then
-          expect(result).toEqual(expectedSiteNavigation.data.body)
-        })
-      })
     })
   })
 })
