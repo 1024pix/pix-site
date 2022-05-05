d0cd6e4a2a1cca4a413850b768e01fb1331c35e1
Vincent Hardouin <vincent.hardouin@pix.fr>
2022-05-05 17:29:02 +0200

    feat: use root url as fallback

--- app/prismic/link-resolver.js
+++ app/prismic/link-resolver.js
@@ -1,22 +1,16 @@
 import { DOCUMENTS } from '~/services/document-fetcher'
 import { translation } from '~/lang'
 
-export const STATIC_ROUTE = [
-  DOCUMENTS.STATISTIQUES,
-  DOCUMENTS.SIMPLE_PAGE,
-  DOCUMENTS.SLICES_PAGE,
-]
 export function linkResolver(doc) {
   const locale = doc.lang !== 'fr-fr' ? `/${doc.lang}` : ''
-  if (STATIC_ROUTE.includes(doc.type)) {
-    return `${locale}/${doc.uid}`
-  }
+
   if (doc.type === DOCUMENTS.NEWS_ITEM) {
     return `${locale}/${translation[doc.lang]['news-page-prefix']}/${doc.uid}`
   }
   if (doc.type === DOCUMENTS.INDEX) {
     return `${locale}/`
   }
+  return `${locale}/${doc.uid}`
 }
 
 export default linkResolver
--- tests/app/prismic/link-resolver.test.js
+++ tests/app/prismic/link-resolver.test.js
@@ -1,36 +1,32 @@
 import { DOCUMENTS } from '~/services/document-fetcher'
-import { linkResolver, STATIC_ROUTE } from '~/app/prismic/link-resolver'
+import { linkResolver } from '~/app/prismic/link-resolver'
 
 describe('linkResolver', () => {
-  describe('when document is for root level', function () {
-    const testCases = [
-      {
-        lang: 'fr-fr',
-        uid: 'test-uid',
-        expectedUrl: '/test-uid',
-      },
-      {
-        lang: 'fr-be',
-        uid: 'test-uid',
-        expectedUrl: '/fr-be/test-uid',
-      },
-      {
-        lang: 'en-gb',
-        uid: 'test-uid',
-        expectedUrl: '/en-gb/test-uid',
-      },
-    ]
+  const testCases = [
+    {
+      lang: 'fr-fr',
+      uid: 'test-uid',
+      expectedUrl: '/test-uid',
+    },
+    {
+      lang: 'fr-be',
+      uid: 'test-uid',
+      expectedUrl: '/fr-be/test-uid',
+    },
+    {
+      lang: 'en-gb',
+      uid: 'test-uid',
+      expectedUrl: '/en-gb/test-uid',
+    },
+  ]
 
-    testCases.forEach(({ lang, uid, expectedUrl }) => {
-      STATIC_ROUTE.forEach((type) => {
-        test(`it should return ${expectedUrl} when type is ${type} and lang is ${lang}`, () => {
-          // when
-          const url = linkResolver({ type, lang, uid })
+  testCases.forEach(({ lang, uid, expectedUrl }) => {
+    test(`it should return ${expectedUrl}`, () => {
+      // when
+      const result = linkResolver({ type: 'anything', lang, uid })
 
-          // then
-          expect(url).toEqual(expectedUrl)
-        })
-      })
+      // then
+      expect(result).toEqual(expectedUrl)
     })
   })
 
@@ -84,33 +80,6 @@ describe('linkResolver', () => {
       test(`it should return root url for lang ${lang}`, () => {
         // when
         const result = linkResolver({ type: DOCUMENTS.INDEX, lang })
-
-        // then
-        expect(result).toEqual(expectedUrl)
-      })
-    })
-  })
-
-  describe('when document type is not specific', () => {
-    const testCases = [
-      {
-        lang: 'fr-fr',
-        expectedUrl: undefined,
-      },
-      {
-        lang: 'fr-be',
-        expectedUrl: undefined,
-      },
-      {
-        lang: 'en-gb',
-        expectedUrl: undefined,
-      },
-    ]
-
-    testCases.forEach(({ lang, expectedUrl }) => {
-      test(`it should return ${expectedUrl}`, () => {
-        // when
-        const result = linkResolver({ type: 'not-defined', lang })
 
         // then
         expect(result).toEqual(expectedUrl)
