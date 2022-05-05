b94ad2d7e8888e9d3d62cd0258c7eb9f1138a20b
Vincent Hardouin <vincent.hardouin@pix.fr>
2022-05-05 13:21:39 +0200

    feat: add missing type SLICES_PAGE in link-resolver

--- app/prismic/link-resolver.js
+++ app/prismic/link-resolver.js
@@ -1,5 +1,9 @@
 import { DOCUMENTS } from '~/services/document-fetcher'
-export const STATIC_ROUTE = [DOCUMENTS.STATISTIQUES, DOCUMENTS.SIMPLE_PAGE]
+export const STATIC_ROUTE = [
+  DOCUMENTS.STATISTIQUES,
+  DOCUMENTS.SIMPLE_PAGE,
+  DOCUMENTS.SLICES_PAGE,
+]
 export function linkResolver(doc) {
   if (STATIC_ROUTE.includes(doc.type)) {
     const locale = doc.lang !== 'fr-fr' ? `/${doc.lang}` : ''
