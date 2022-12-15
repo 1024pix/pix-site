19f374759c702c2f559e4e62bdd2df668ab2bb1f
Vincent Hardouin <vincent.hardouin@pix.fr>
2022-12-15 15:28:29 +0100

    sr: add snapshot test for language switcher

--- tests/components/LanguageSwitcher.test.js
+++ tests/components/LanguageSwitcher.test.js
@@ -1,66 +1,103 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, mount } from '@vue/test-utils'
 import LanguageSwitcher from '~/components/LanguageSwitcher/LanguageSwitcher'
+import LanguageSwitcherSubMenu from '~/components/LanguageSwitcher/LanguageSwitcherSubMenu.vue'
 
 describe('Component: LanguageSwitcher', () => {
-  it('should render the language switcher if site is pix-pro.fr', () => {
-    // given
-    const $t = () => {}
-    const $i18n = { locale: 'fr-fr' }
+  let oldDomainFr
+  let oldDomainOrg
 
-    // when
-    const wrapper = shallowMount(LanguageSwitcher, {
-      stubs: {
-        LanguageSwitcherSubMenu: true,
-        fa: true,
-      },
-      propsData: {
-        type: 'with-dropdown',
-      },
-      data: LanguageSwitcher.data,
-      computed: LanguageSwitcher.computed,
-      mocks: { $t, $i18n },
-    })
-
-    // then
-    expect(wrapper.find('.language-switcher__button').exists()).toBe(true)
+  beforeEach(() => {
+    oldDomainFr = process.env.DOMAIN_FR
+    oldDomainOrg = process.env.DOMAIN_ORG
+    process.env.DOMAIN_FR = 'example.fr'
+    process.env.DOMAIN_ORG = 'example.org'
   })
 
-  it('should render the language switcher if site is pix-pro.org', async () => {
-    // given
-    const $t = () => {}
-    const $i18n = { locale: 'fr' }
+  afterEach(() => {
+    process.env.DOMAIN_FR = oldDomainFr
+    process.env.DOMAIN_ORG = oldDomainOrg
+  })
 
-    const LanguageSwitcherSubMenuStub = {
-      name: 'language-switcher-burger-menu',
-      template: '<div class="language-switcher__dropdown-menu child"/>',
-    }
-
-    const wrapper = shallowMount(LanguageSwitcher, {
-      stubs: {
-        LanguageSwitcherSubMenu: LanguageSwitcherSubMenuStub,
-        fa: true,
-      },
-      propsData: {
-        type: 'with-dropdown',
-      },
-      data: LanguageSwitcher.data,
-      computed: LanguageSwitcher.computed,
-      mocks: { $t, $i18n },
+  describe('when site is pix-pro', () => {
+    let oldSite
+    beforeEach(() => {
+      oldSite = process.env.SITE
+      process.env.SITE = 'pix-pro'
     })
 
-    const languageSwitcher = wrapper.find('.language-switcher__button')
+    afterEach(() => {
+      process.env.SITE = oldSite
+    })
 
-    // when
-    await languageSwitcher.trigger('click')
-    const languageChildrenSubMenuButton = wrapper.find(
-      '.language-switcher-sub-menu-button'
-    )
-    await languageChildrenSubMenuButton.trigger('click')
-    const languageChildrenSubMenu = wrapper.find(
-      '.language-switcher__dropdown-menu.child'
-    )
+    it('should render the language switcher', () => {
+      // given
+      const $t = () => {}
+      const $i18n = { locale: 'fr-fr' }
 
-    // then
-    expect(languageChildrenSubMenu.exists()).toBe(true)
+      // when
+      const wrapper = shallowMount(LanguageSwitcher, {
+        stubs: {
+          LanguageSwitcherSubMenu: true,
+          fa: true,
+        },
+        propsData: {
+          type: 'with-dropdown',
+        },
+        data: LanguageSwitcher.data,
+        computed: LanguageSwitcher.computed,
+        mocks: { $t, $i18n },
+      })
+
+      // then
+      expect(wrapper.find('.language-switcher__button').exists()).toBe(true)
+    })
+
+    it('should render the language switcher correctly for pix-pro', async () => {
+      // given
+      const $t = () => {}
+      const $i18n = { locale: 'fr-fr' }
+
+      // when
+      const wrapper = mount(LanguageSwitcher, {
+        components: { LanguageSwitcherSubMenu },
+        mocks: { $t, $i18n },
+        stubs: { fa: true },
+      })
+      await wrapper.find('button').trigger('click')
+      await wrapper.find('.language-switcher-sub-menu-button').trigger('click')
+
+      // then
+      expect(wrapper.element).toMatchSnapshot()
+    })
+  })
+
+  describe('when site is pix-site', () => {
+    let oldSite
+    beforeEach(() => {
+      oldSite = process.env.SITE
+      process.env.SITE = 'pix-site'
+    })
+
+    afterEach(() => {
+      process.env.SITE = oldSite
+    })
+
+    it('should render the language switcher correctly for pix-site', async () => {
+      // given
+      const $t = () => {}
+      const $i18n = { locale: 'fr-fr' }
+
+      // when
+      const wrapper = mount(LanguageSwitcher, {
+        components: { LanguageSwitcherSubMenu },
+        mocks: { $t, $i18n },
+        stubs: { fa: true },
+      })
+      await wrapper.find('button').trigger('click')
+      await wrapper.find('.language-switcher-sub-menu-button').trigger('click')
+
+      // then
+      expect(wrapper.element).toMatchSnapshot()
+    })
   })
 })
