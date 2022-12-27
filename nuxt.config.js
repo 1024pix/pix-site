4bc2b60ecb4a467c0a422e994c546ce09ca98ea8
Vincent Hardouin <vincent.hardouin@pix.fr>
2022-12-27 10:39:16 +0100

    feat: add root redirection to locale-choice

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,8 @@ const i18nConfigurationForInternationalDomain = {
   vueI18n: {
     fallbackLocale: 'fr',
   },
+  rootRedirect:
+    config.isPixSite && !config.isFrenchDomain ? 'locale-choice' : undefined,
 }
 
 const nuxtConfig = {
