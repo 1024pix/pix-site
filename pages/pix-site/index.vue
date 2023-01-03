d7bfef500b54cf46757bf7a02291793facd90ec9
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2023-01-03 18:00:07 +0100

    do not pass through /locale-choice page

--- pages/pix-site/index.vue
+++ pages/pix-site/index.vue
@@ -1,14 +1,41 @@
-<template><div></div></template>
+<template>
+  <client-only v-if="shouldDisplayLocaleChoice">
+    <div class="locale-choice">
+      <h1>
+        <pix-image
+          class="logo-pix"
+          :field="{ url: '/images/logo-pix-blanc.svg', alt: 'Pix' }"
+        />
+      </h1>
+      <LocaleLink
+        v-for="locale in locales"
+        :key="locale.code"
+        class="locale-choice__link"
+        :locale="locale"
+      />
+      <pix-image class="planet" :field="{ url: '/images/planet.svg' }" />
+    </div>
+  </client-only>
+</template>
 
 <script>
+import { language } from '~/config/language'
 export default {
+  layout: 'home',
   nuxtI18n: false,
-  layout: 'simple',
+  data() {
+    return {
+      locales: language.locales,
+      shouldDisplayLocaleChoice: false,
+    }
+  },
   mounted() {
     const preferredLocale = this.getLocaleFromCookie()
-    document.location = preferredLocale
-      ? `/${preferredLocale}`
-      : '/locale-choice'
+    if (preferredLocale) {
+      return this.$router.push(preferredLocale)
+    } else {
+      this.shouldDisplayLocaleChoice = true
+    }
   },
   methods: {
     getLocaleFromCookie() {
