8e1440390422f8d06596dbc6d48632d3ecf0c828
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2023-01-03 18:25:48 +0100

    remove home layout to avoid CLS

--- pages/pix-site/index.vue
+++ pages/pix-site/index.vue
@@ -1,27 +1,29 @@
 <template>
   <client-only v-if="shouldDisplayLocaleChoice">
-    <div class="locale-choice">
-      <h1>
-        <pix-image
-          class="logo-pix"
-          :field="{ url: '/images/logo-pix-blanc.svg', alt: 'Pix' }"
+    <main class="home">
+      <div class="locale-choice">
+        <h1>
+          <pix-image
+            class="logo-pix"
+            :field="{ url: '/images/logo-pix-blanc.svg', alt: 'Pix' }"
+          />
+        </h1>
+        <LocaleLink
+          v-for="locale in locales"
+          :key="locale.code"
+          class="locale-choice__link"
+          :locale="locale"
         />
-      </h1>
-      <LocaleLink
-        v-for="locale in locales"
-        :key="locale.code"
-        class="locale-choice__link"
-        :locale="locale"
-      />
-      <pix-image class="planet" :field="{ url: '/images/planet.svg' }" />
-    </div>
+        <pix-image class="planet" :field="{ url: '/images/planet.svg' }" />
+      </div>
+    </main>
   </client-only>
 </template>
 
 <script>
 import { language } from '~/config/language'
 export default {
-  layout: 'home',
+  layout: 'empty',
   nuxtI18n: false,
   data() {
     return {
@@ -32,7 +34,7 @@ export default {
   mounted() {
     const preferredLocale = this.getLocaleFromCookie()
     if (preferredLocale) {
-      return this.$router.push(preferredLocale)
+      document.location = `/${preferredLocale}/`
     } else {
       this.shouldDisplayLocaleChoice = true
     }
@@ -48,3 +50,21 @@ export default {
   },
 }
 </script>
+
+<style lang="scss" scoped>
+.home {
+  width: 100%;
+  min-width: 0;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 5rem 1rem 0;
+  background: url('/images/stars.svg') repeat, $default-gradient;
+
+  @include device-is('large-mobile') {
+    padding: 0;
+  }
+}
+</style>
