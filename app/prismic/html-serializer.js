92750d34d91f8fcd270bc949ff7ea186772c3ee6
Vincent Hardouin <vincent.hardouin@pix.fr>
2023-01-26 16:13:25 +0100

    sr: use pix-prismic-link in html-serializer

--- app/prismic/html-serializer.js
+++ app/prismic/html-serializer.js
@@ -1,5 +1,4 @@
 import prismicDOM from 'prismic-dom'
-import linkResolver from './link-resolver'
 
 const Elements = prismicDOM.RichText.Elements
 
@@ -7,18 +6,7 @@ export default function (type, element, content, children) {
   // Generate links to Prismic Documents as <router-link> components
   // Present by default, it is recommended to keep this
   if (type === Elements.hyperlink) {
-    let result = ''
-    const url = prismicDOM.Link.url(element.data, linkResolver)
-
-    if (element.data.link_type === 'Document') {
-      result = `<a href="${url}" data-nuxt-link>${content}</a>`
-    } else {
-      const target = element.data.target
-        ? `target="'${element.data.target}'" rel="noopener"`
-        : ''
-      result = `<a href="${url}" ${target}>${content}</a>`
-    }
-    return result
+    return `<pix-prismic-link field="${element.data}">${content}</pix-prismic-link>`
   }
 
   // If the image is also a link to a Prismic Document, it will return a <router-link> component
@@ -29,16 +17,7 @@ export default function (type, element, content, children) {
     }" copyright="${element.copyright || ''}">`
 
     if (element.linkTo) {
-      const url = prismicDOM.Link.url(element.linkTo, linkResolver)
-
-      if (element.linkTo.link_type === 'Document') {
-        result = `<nuxt-link to="${url}">${result}</nuxt-link>`
-      } else {
-        const target = element.linkTo.target
-          ? `target="${element.linkTo.target}" rel="noopener"`
-          : ''
-        result = `<a href="${url}" ${target}>${result}</a>`
-      }
+      result = `<pix-prismic-link field="${element.linkTo}">${result}</pix-prismic-link>`
     }
     const wrapperClassList = [element.label || '', 'block-img']
     result = `<p class="${wrapperClassList.join(' ')}">${result}</p>`
