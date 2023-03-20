2352e0154e18f26fca4aaa1b3afcab918491a2fe
Eric Lim <eric.lim@zenika.com>
2023-03-20 16:34:51 +0100

    feat: connect get-locale-from-cookie service with index components

--- tests/pages/pix-pro/index.test.js
+++ tests/pages/pix-pro/index.test.js
@@ -1,6 +1,18 @@
 import { shallowMount } from '@vue/test-utils'
 import Index from '@/pages/pix-pro/index.vue'
 
+jest.mock('~/config/environment', () => {
+  return {
+    config: {
+      isPixSite: false,
+      domain: {
+        french: 'pix.fr',
+        international: 'pix.org',
+      },
+    },
+  }
+})
+
 describe('Index Page', () => {
   let $router
 
@@ -33,7 +45,7 @@ describe('Index Page', () => {
         // then
         expect($router.replace).not.toHaveBeenCalled()
         const localeLinks = wrapper.findAll('locale-link-stub')
-        expect(localeLinks.length).toBe(4)
+        expect(localeLinks.length).toBe(3)
       })
     })
 
@@ -57,29 +69,32 @@ describe('Index Page', () => {
         })
       })
 
-      describe('with value being in canonical BCP 47 format', () => {
-        test('redirects to correct locale page', async () => {
-          // given
-          document.cookie = 'foo=bar; locale=en-GB'
+      describe('with value equal to en-GB or in lowercase format', () => {
+        const locales = ['en-gb', 'en-GB']
+        locales.forEach((locale) => {
+          test('redirects to correct locale page', async () => {
+            // given
+            document.cookie = `foo=bar; locale=${locale}`
 
-          // when
-          const wrapper = shallowMount(Index, {
-            mocks: { $router },
-            stubs: ['client-only', 'pix-image', 'locale-link'],
+            // when
+            const wrapper = shallowMount(Index, {
+              mocks: { $router },
+              stubs: ['client-only', 'pix-image', 'locale-link'],
+            })
+            await wrapper.vm.$nextTick()
+
+            // then
+            expect($router.replace).toHaveBeenCalledWith(`/en/`)
+            const localeLinks = wrapper.findAll('locale-link-stub')
+            expect(localeLinks.length).toBe(0)
           })
-          await wrapper.vm.$nextTick()
-
-          // then
-          expect($router.replace).toHaveBeenCalledWith('/en-gb/')
-          const localeLinks = wrapper.findAll('locale-link-stub')
-          expect(localeLinks.length).toBe(0)
         })
       })
 
       describe('in lowercase', () => {
         test('redirects to correct locale page', async () => {
           // given
-          document.cookie = 'foo=bar; locale=en-gb'
+          document.cookie = 'foo=bar; locale=fr'
 
           // when
           const wrapper = shallowMount(Index, {
@@ -89,100 +104,9 @@ describe('Index Page', () => {
           await wrapper.vm.$nextTick()
 
           // then
-          expect($router.replace).toHaveBeenCalledWith('/en-gb/')
+          expect($router.replace).toHaveBeenCalledWith('/fr/')
           const localeLinks = wrapper.findAll('locale-link-stub')
           expect(localeLinks.length).toBe(0)
-        })
-      })
-    })
-  })
-
-  describe('#methods', () => {
-    describe('#getLocaleFromCookie', () => {
-      describe('when there is no cookie', () => {
-        test('returns no locale', () => {
-          // given
-          document.cookie = ''
-          const wrapper = shallowMount(Index, {
-            mocks: { $router },
-            stubs: ['client-only', 'pix-image', 'locale-link'],
-          })
-
-          // when
-          const chosenLocale = wrapper.vm.getLocaleFromCookie()
-
-          // then
-          expect(chosenLocale).toBe(null)
-        })
-      })
-
-      describe('when there is a locale cookie', () => {
-        describe('with value being only one subtag', () => {
-          test('returns the proper locale', () => {
-            // given
-            document.cookie = 'foo=bar; locale=fr'
-            const wrapper = shallowMount(Index, {
-              mocks: { $router },
-              stubs: ['client-only', 'pix-image', 'locale-link'],
-            })
-
-            // when
-            const chosenLocale = wrapper.vm.getLocaleFromCookie()
-
-            // then
-            expect(chosenLocale).toBe('fr')
-          })
-        })
-
-        describe('with value being in canonical BCP 47 format', () => {
-          test('returns the proper locale in lowercase', () => {
-            // given
-            document.cookie = 'foo=bar; locale=en-GB'
-            const wrapper = shallowMount(Index, {
-              mocks: { $router },
-              stubs: ['client-only', 'pix-image', 'locale-link'],
-            })
-
-            // when
-            const chosenLocale = wrapper.vm.getLocaleFromCookie()
-
-            // then
-            expect(chosenLocale).toBe('en-gb')
-          })
-        })
-
-        describe('in lowercase', () => {
-          test('returns the proper locale', () => {
-            // given
-            document.cookie = 'foo=bar; locale=en-gb'
-            const wrapper = shallowMount(Index, {
-              mocks: { $router },
-              stubs: ['client-only', 'pix-image', 'locale-link'],
-            })
-
-            // when
-            const chosenLocale = wrapper.vm.getLocaleFromCookie()
-
-            // then
-            expect(chosenLocale).toBe('en-gb')
-          })
-        })
-      })
-
-      describe('with a crafted cookie', () => {
-        test('cookie value is ignored', () => {
-          // given
-          document.cookie = 'foo=bar; locale=1234-crafted-cookie'
-          const wrapper = shallowMount(Index, {
-            mocks: { $router },
-            stubs: ['client-only', 'pix-image', 'locale-link'],
-          })
-
-          // when
-          const chosenLocale = wrapper.vm.getLocaleFromCookie()
-
-          // then
-          expect(chosenLocale).toBe(null)
         })
       })
     })
--- tests/pages/pix-site/index.test.js
+++ tests/pages/pix-site/index.test.js
@@ -1,6 +1,18 @@
 import { shallowMount } from '@vue/test-utils'
 import Index from '@/pages/pix-site/index.vue'
 
+jest.mock('~/config/environment', () => {
+  return {
+    config: {
+      isPixSite: true,
+      domain: {
+        french: 'pix.fr',
+        international: 'pix.org',
+      },
+    },
+  }
+})
+
 describe('Index Page', () => {
   let $router
 
@@ -57,132 +69,49 @@ describe('Index Page', () => {
         })
       })
 
-      describe('with value being in canonical BCP 47 format', () => {
-        test('redirects to correct locale page', async () => {
-          // given
-          document.cookie = 'foo=bar; locale=en-GB'
-
-          // when
-          const wrapper = shallowMount(Index, {
-            mocks: { $router },
-            stubs: ['client-only', 'pix-image', 'locale-link'],
-          })
-          await wrapper.vm.$nextTick()
-
-          // then
-          expect($router.replace).toHaveBeenCalledWith('/en-gb/')
-          const localeLinks = wrapper.findAll('locale-link-stub')
-          expect(localeLinks.length).toBe(0)
-        })
-      })
-
-      describe('in lowercase', () => {
-        test('redirects to correct locale page', async () => {
-          // given
-          document.cookie = 'foo=bar; locale=en-gb'
-
-          // when
-          const wrapper = shallowMount(Index, {
-            mocks: { $router },
-            stubs: ['client-only', 'pix-image', 'locale-link'],
-          })
-          await wrapper.vm.$nextTick()
-
-          // then
-          expect($router.replace).toHaveBeenCalledWith('/en-gb/')
-          const localeLinks = wrapper.findAll('locale-link-stub')
-          expect(localeLinks.length).toBe(0)
-        })
-      })
-    })
-  })
-
-  describe('#methods', () => {
-    describe('#getLocaleFromCookie', () => {
-      describe('when there is no cookie', () => {
-        test('returns no locale', () => {
-          // given
-          document.cookie = ''
-          const wrapper = shallowMount(Index, {
-            mocks: { $router },
-            stubs: ['client-only', 'pix-image', 'locale-link'],
-          })
-
-          // when
-          const chosenLocale = wrapper.vm.getLocaleFromCookie()
-
-          // then
-          expect(chosenLocale).toBe(null)
-        })
-      })
-
-      describe('when there is a locale cookie', () => {
-        describe('with value being only one subtag', () => {
-          test('returns the proper locale', () => {
+      describe('with value equal to en-GB or in lowercase format', () => {
+        const locales = ['en-gb', 'en-GB']
+        locales.forEach((locale) => {
+          test('redirects to correct locale page', async () => {
             // given
-            document.cookie = 'foo=bar; locale=fr'
+            document.cookie = `foo=bar; locale=${locale}`
+
+            // when
             const wrapper = shallowMount(Index, {
               mocks: { $router },
               stubs: ['client-only', 'pix-image', 'locale-link'],
             })
-
-            // when
-            const chosenLocale = wrapper.vm.getLocaleFromCookie()
+            await wrapper.vm.$nextTick()
 
             // then
-            expect(chosenLocale).toBe('fr')
-          })
-        })
-
-        describe('with value being in canonical BCP 47 format', () => {
-          test('returns the proper locale in lowercase', () => {
-            // given
-            document.cookie = 'foo=bar; locale=en-GB'
-            const wrapper = shallowMount(Index, {
-              mocks: { $router },
-              stubs: ['client-only', 'pix-image', 'locale-link'],
-            })
-
-            // when
-            const chosenLocale = wrapper.vm.getLocaleFromCookie()
-
-            // then
-            expect(chosenLocale).toBe('en-gb')
-          })
-        })
-
-        describe('in lowercase', () => {
-          test('returns the proper locale', () => {
-            // given
-            document.cookie = 'foo=bar; locale=en-gb'
-            const wrapper = shallowMount(Index, {
-              mocks: { $router },
-              stubs: ['client-only', 'pix-image', 'locale-link'],
-            })
-
-            // when
-            const chosenLocale = wrapper.vm.getLocaleFromCookie()
-
-            // then
-            expect(chosenLocale).toBe('en-gb')
+            expect($router.replace).toHaveBeenCalledWith(`/en/`)
+            const localeLinks = wrapper.findAll('locale-link-stub')
+            expect(localeLinks.length).toBe(0)
           })
         })
       })
 
-      describe('with a crafted cookie', () => {
-        test('cookie value is ignored', () => {
-          // given
-          document.cookie = 'foo=bar; locale=1234-crafted-cookie'
-          const wrapper = shallowMount(Index, {
-            mocks: { $router },
-            stubs: ['client-only', 'pix-image', 'locale-link'],
+      describe('with value being in canonical BCP 47 or lowercase format', () => {
+        const locales = ['fr-be', 'fr-BE']
+        locales.forEach((locale) => {
+          test('redirects to correct locale page', async () => {
+            // given
+            document.cookie = `foo=bar; locale=${locale}`
+
+            // when
+            const wrapper = shallowMount(Index, {
+              mocks: { $router },
+              stubs: ['client-only', 'pix-image', 'locale-link'],
+            })
+            await wrapper.vm.$nextTick()
+
+            // then
+            expect($router.replace).toHaveBeenCalledWith(
+              `/${locale.toLowerCase()}/`
+            )
+            const localeLinks = wrapper.findAll('locale-link-stub')
+            expect(localeLinks.length).toBe(0)
           })
-
-          // when
-          const chosenLocale = wrapper.vm.getLocaleFromCookie()
-
-          // then
-          expect(chosenLocale).toBe(null)
         })
       })
     })
