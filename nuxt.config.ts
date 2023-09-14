60faa265f51f1f3b0795e45de344f7eaf468cdbc
Geoffroy Begouaussel <geoffroy.begouaussel@zenika.com>
2023-09-14 14:58:21 +0200

    refactor(Vue3): add prismic slicemachine

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/prismic"],
+
+  prismic: {
+    endpoint: "pix-site"
+  }
 })
---
+++ pages/slice-simulator.vue
@@ -0,0 +1,10 @@
+<template>
+  <SliceSimulator #default="{ slices }">
+    <SliceZone :slices="slices" :components="components" />
+  </SliceSimulator>
+</template>
+
+<script setup lang="ts">
+import { SliceSimulator } from "@slicemachine/adapter-nuxt/simulator";
+import { components } from "~/slices";
+</script>
