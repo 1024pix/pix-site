f8a3382cbfae3088ab2d93d27afd70e6da176d7b
Geoffroy Begouaussel <geoffroy.begouaussel@zenika.com>
2023-09-26 15:11:21 +0200

    config imports from shared folder

--- pix-site/nuxt.config.ts
+++ pix-site/nuxt.config.ts
@@ -1,12 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { resolve } from "path";
 import i18nConfig from "./i18n.config";
 
 export default defineNuxtConfig({
+  alias: {
+    "@shared": resolve(__dirname, "../shared"),
+  },
   devtools: { enabled: true },
   modules: ["@nuxtjs/prismic", "@nuxtjs/i18n", "@vueuse/nuxt", "@nuxt/image"],
   i18n: i18nConfig,
+  components: ["@/components", "@shared/components"],
   /* SCSS global imports */
-  css: ["@/assets/scss/shared.scss"],
+  css: ["@shared/assets/scss/shared.scss"],
   vite: {
     css: {
       preprocessorOptions: {
