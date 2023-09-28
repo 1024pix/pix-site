3c93092b8cd875a83cc55bdf2c092362d4d187b6
Geoffroy Begouaussel <geoffroy.begouaussel@zenika.com>
2023-09-28 15:22:33 +0200

    add useEnvironmentUrl test

--- shared/hooks/useEnvironmentUrl.ts
+++ shared/hooks/useEnvironmentUrl.ts
@@ -1,12 +1,17 @@
 import { useRuntimeConfig } from "#imports";
 
 export default function useEnvironmentUrl() {
-  const getEnvironmentUrl = (url) => {
+  const getEnvironmentUrl = (url: string) => {
     const runtimeConfig = useRuntimeConfig();
+
+    const currentSite = runtimeConfig.public.site
+      ? String(runtimeConfig.public.site)
+      : null;
 
     if (
       process.env.NODE_ENV !== "production" &&
-      url.includes(runtimeConfig.public.site)
+      !!currentSite &&
+      url.includes(currentSite)
     ) {
       return url.replace(/^.*\/\/[^\/]+/, "");
     }
