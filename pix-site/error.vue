10c6fc447da9ee21adf34cbf15b31294ac227007
Geoffroy Begouaussel <geoffroy.begouaussel@zenika.com>
2023-09-29 15:24:05 +0200

    add 404 error page

---
+++ pix-site/error.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="error">
+    <a :href="t('home-page-url')">
+      <img
+        class="logo"
+        src="/images/pix-logo.svg"
+        alt="Lien pour revenir à l'accueil"
+      />
+    </a>
+    <div v-html="t('error-content')" />
+  </div>
+</template>
+
+<script setup>
+const { locale: i18nLocale, t } = useI18n();
+
+const props = defineProps({
+  error: Object,
+});
+
+useHead({
+  title: "Error | Pix",
+  htmlAttrs: {
+    lang: i18nLocale,
+  },
+});
+</script>
+
+<style lang="scss">
+.error {
+  width: 300px;
+  margin-left: auto;
+  margin-right: auto;
+
+  @include device-is("tablet") {
+    width: 500px;
+  }
+
+  .logo {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+  }
+}
+</style>
