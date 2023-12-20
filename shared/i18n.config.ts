5bd7594e403746c6087c6087763107edf530f887
Matthias FERRAINA <matthias.ferraina@pix.fr>
2023-12-20 10:53:30 +0100

    fix: locale switcher
    Co-authored-by: Vincent Hardouin <vincent.hardouin@pix.fr>

--- shared/i18n.config.ts
+++ shared/i18n.config.ts
@@ -7,21 +7,21 @@ const config = {
       file: "en.js",
       name: "English",
       icon: "globe-europe.svg",
-      newDomain: `http://${process.env.DOMAIN_ORG}`
+      domain: `http://${process.env.DOMAIN_ORG}`
     },
     {
       code: "fr",
       file: "fr.js",
       name: "Français",
       icon: "globe-europe.svg",
-      newDomain: `http://${process.env.DOMAIN_ORG}`
+      domain: `http://${process.env.DOMAIN_ORG}`
     },
     {
       code: "fr-fr",
       file: "fr-fr.js",
       name: "France",
       icon: "flag-fr.svg",
-      newDomain: `http://${process.env.DOMAIN_FR}`
+      domain: `http://${process.env.DOMAIN_FR}`
     }
   ],
   strategy: "prefix_except_default",
@@ -40,7 +40,7 @@ if (process.env.SITE === "pix-site") {
     file: "fr-be.js",
     name: "Fédération Wallonie-Bruxelles",
     icon: "flag-be.svg",
-    newDomain: `http://${process.env.DOMAIN_ORG}`
+    domain: `http://${process.env.DOMAIN_ORG}`
   });
 }
 
