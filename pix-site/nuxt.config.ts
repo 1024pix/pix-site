b77e951cae4575412062b7dd054e41284bcf3b07
Eric Lim <eric.lim@zenika.com>
2024-02-28 11:17:40 +0100

    feat(site): add testing configuration for pix-site and pix-pro
    Co-authored-by: Ismael Gorissen <ismael.gorissen@gmail.com>
    Co-authored-by: Rébecca Kaci <rebecca.kaci@gmail.com>
    Co-authored-by: Emmanuelle Bonnemay <emmanuelle.bonnemay@pix.fr>

--- pix-site/nuxt.config.ts
+++ pix-site/nuxt.config.ts
@@ -1,7 +1,7 @@
 import { getRoutesToGenerate } from "./services/get-routes-to-generate";
 import i18nConfig from "./i18n.config";
 export default async () => {
-  const routes = await getRoutesToGenerate({ locales: i18nConfig.locales });
+  const routes = process.env.NODE_ENV !== 'test' ? await getRoutesToGenerate({ locales: i18nConfig.locales }) : [];
   return defineNuxtConfig({
       extends: ["../shared"],
       devServer: {
---
+++ pix-site/vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineVitestConfig } from '@nuxt/test-utils/config'
+import { configDefaults} from "vitest/config";
+
+export default defineVitestConfig({
+  test: {
+    environment: 'nuxt',
+    exclude: [
+      ...configDefaults.exclude, '**/e2e/**'
+    ],
+    globals: true,
+  },
+})
+
