15e3e2d8a8257c6d55e0d07e6376599cf96de2e5
Geoffroy Begouaussel <geoffroy.begouaussel@gmail.com>
2024-02-29 17:57:49 +0100

    Revert "[TECH] Sécuriser l'API Prismic avec un token (PIX-11429)."

--- pix-site/services/get-routes-to-generate.js
+++ pix-site/services/get-routes-to-generate.js
@@ -1,39 +1,38 @@
-import * as prismic from '@prismicio/client'
-import { linkResolver } from '../../shared/services/link-resolver.js'
+import * as prismic from "@prismicio/client";
+import { linkResolver } from "../../shared/services/link-resolver.js";
 
 export const getRoutesToGenerate = async function({ locales }) {
-  console.log(process.env.PRISMIC_API_TOKEN)
-  const client = await prismic.createClient(process.env.PRISMIC_API_ENDPOINT, { 'accessToken': process.env.PRISMIC_API_TOKEN })
-  const { routes, totalPages } = await getRoutesInPage(client, 1, locales)
+  const client = await prismic.createClient("https://pix-site.cdn.prismic.io/api/v2");
+  const { routes, totalPages } = await getRoutesInPage(client, 1, locales);
 
   for (let page = 2; page <= totalPages; page++) {
-    const { routes: nextPageRoutes } = await getRoutesInPage(client, page, locales)
-    routes.push(...nextPageRoutes)
+    const { routes: nextPageRoutes } = await getRoutesInPage(client, page, locales);
+    routes.push(...nextPageRoutes);
   }
 
-  console.info(`${routes.length} routes will be generated`)
-  return routes
-}
+  console.info(`${routes.length} routes will be generated`);
+  return routes;
+};
 
 async function getRoutesInPage(prismicClient, page, locales) {
   const { results, total_pages: totalPages } = await prismicClient.get({
       filters: [
-        prismic.filter.at('document.tags', ['pix-site']),
-        prismic.filter.not('document.tags', ['fragment'])
+        prismic.filter.at("document.tags", ["pix-site"]),
+        prismic.filter.not("document.tags", ["fragment"])
       ],
       pageSize: 100,
       page,
-      lang: '*'
+      lang: "*"
     }
-  )
+  );
   const localesToBuild = locales.map(
     ({ code }) => code
-  )
+  );
 
   const routes = results
     .filter(({ uid }) => Boolean(uid))
     .filter(({ lang }) => localesToBuild.includes(lang))
-    .map(linkResolver)
+    .map(linkResolver);
 
   return { totalPages, routes }
 }
