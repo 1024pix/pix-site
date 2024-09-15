df2be180560c81dfe77b058ecfbb16313dc43483
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2024-09-15 21:53:55 +0200

    feat(shared): rewrite URLs on the fly

--- shared/composables/useEnvironmentUrl.ts
+++ shared/composables/useEnvironmentUrl.ts
@@ -1,17 +1,35 @@
+const FR_FR_LOCALE_CODE = 'fr-fr';
+
 export default function useEnvironmentUrl() {
   const getEnvironmentUrl = (url: string) => {
     const runtimeConfig = useRuntimeConfig();
 
-    const currentSite = runtimeConfig.public.site ? String(runtimeConfig.public.site) : null;
+    const siteUrlStart = runtimeConfig.public.site as string;
+    const availableLocaleCodes: Array<string> = runtimeConfig.public.availableLocaleCodes as Array<string>;
 
-    if (process.env.NODE_ENV !== 'production' && !!currentSite && url.includes(currentSite)) {
-      const formatedUrl = url.replace(/^.*\/\/[^/]+/, '');
-      const isHomePage = formatedUrl.length > 0;
-      return isHomePage ? formatedUrl : '/';
+    const urlObj = new URL(url);
+    const prefix = urlObj.pathname.split('/')[1];
+
+    // This is an URL from pix-site or pix-pro
+    if (url.startsWith(siteUrlStart)) {
+      // Adding the /fr-fr prefix if no locale prefix is found
+      // TODO: Remove this block when there is no more
+      // https://pix.fr/ or https://pro.pix.fr/ links in Prismic
+      if (!availableLocaleCodes.includes(prefix)) {
+        urlObj.pathname = `${FR_FR_LOCALE_CODE}${urlObj.pathname}`;
+        url = urlObj.href;
+      }
+
+      // For development and test environments
+      if (process.env.NODE_ENV !== 'production') {
+        url = urlObj.pathname;
+      }
     }
 
     return url;
   };
 
-  return { getEnvironmentUrl };
+  return {
+    getEnvironmentUrl,
+  };
 }
