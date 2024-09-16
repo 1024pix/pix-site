56d46c95e6af7ccc9ae5895c3c859cd14615d381
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2024-09-16 18:11:25 +0200

    test(pix-site): adapt e2e tests

--- pix-site/tests/e2e/layout/footer.spec.ts
+++ pix-site/tests/e2e/layout/footer.spec.ts
@@ -2,11 +2,11 @@ import { test, expect } from '@playwright/test';
 
 test.describe('layout/footer', () => {
   test('visible elements', async ({ page }) => {
-    await page.goto('/', {
+    await page.goto('/fr-fr/', {
       waitUntil: 'networkidle',
     });
-    await page.getByText('France').click();
-    await page.waitForLoadState('networkidle');
+    // await page.getByText('France').click();
+    // await page.waitForLoadState('networkidle');
 
     const siteFooter = page.getByRole('contentinfo');
 
