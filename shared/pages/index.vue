8d5ba84550c3eca4e9b5294d6c9147fae58e2c4e
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2024-10-02 14:24:57 +0200

    feat(shared): leverage new getBestMatchingLocaleName function

--- shared/pages/index.vue
+++ shared/pages/index.vue
@@ -10,12 +10,14 @@ definePageMeta({
   layout: 'empty',
 });
 
-const { localeCookie } = useLocaleCookie();
+const { localeCookie, getBestMatchingLocaleName } = useLocaleCookie();
 const locale = localeCookie.value;
+
 onBeforeMount(() => {
   const router = useRouter();
+
   if (locale) {
-    return router.replace(`/${locale}/`);
+    return router.replace(`/${getBestMatchingLocaleName(locale)}/`);
   }
 });
 </script>
