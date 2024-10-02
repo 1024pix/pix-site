b12c094be5d0435e4c56d9619cbd0be4289e1ca6
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2024-10-02 11:26:02 +0200

    refactor(shared): improve code clarity

--- shared/composables/useLocaleCookie.ts
+++ shared/composables/useLocaleCookie.ts
@@ -1,26 +1,30 @@
+const LOCALE_COOKIE_NAME = 'locale';
+
 export default function useLocaleCookie() {
   const appConfig = useAppConfig();
   const runtimeConfig = useRuntimeConfig();
   const domainFrUrl = new URL(appConfig.domainFr);
   const domainOrgUrl = new URL(appConfig.domainOrg);
 
-  const previousLocaleCookie = useCookie('locale', {
-    maxAge: 31536000,
+  const previousLocaleCookieToDelete = useCookie(LOCALE_COOKIE_NAME, {
+    maxAge: 31536000, // 1 year
     sameSite: 'strict',
   });
 
-  const localeCookie = useCookie('locale', {
+  const localeCookie = useCookie(LOCALE_COOKIE_NAME, {
     domain: runtimeConfig.public.siteDomain === 'ORG' ? domainOrgUrl.hostname : domainFrUrl.hostname,
-    maxAge: 31536000,
+    maxAge: 31536000, // 1 year
     sameSite: 'strict',
   });
 
   // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
   function setLocaleCookie(locale: string, callback?: Function): void {
     const localeCanonicalName = Intl.getCanonicalLocales(locale)?.[0];
-    if (previousLocaleCookie.value) {
-      previousLocaleCookie.value = null;
+
+    if (previousLocaleCookieToDelete.value) {
+      previousLocaleCookieToDelete.value = null;
     }
+
     localeCookie.value = localeCanonicalName;
     if (callback) callback();
   }
