ed0c0c58d51631929b84e9d370692cef1fd72085
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2025-03-11 14:46:50 +0100

    feat(pix-pro): use canonical locales by default

--- pix-pro/components/LocaleSwitcher.vue
+++ pix-pro/components/LocaleSwitcher.vue
@@ -67,7 +67,7 @@ const { localeProperties, t } = useI18n();
 
 const frLocale = availableLocales.find(l => l.code === 'fr');
 const enLocale = availableLocales.find(l => l.code === 'en');
-const frFrLocale = availableLocales.find(l => l.code === 'fr-fr');
+const frFrLocale = availableLocales.find(l => l.code === 'fr-FR');
 
 const buttonRef = ref(null);
 const localesMenuRef = ref(null);
