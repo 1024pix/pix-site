811383c952c99767b734da45eced566991c8a343
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2026-09-16 18:43:20 +0200

    fix(shared): add missing locales in the linkResolver
    Co-authored-by: Nicolas Lepage <nicolas.lepage@pix.fr>

--- shared/services/link-resolver.js
+++ shared/services/link-resolver.js
@@ -2,6 +2,9 @@ import * as en from '../translations/en.js';
 import * as fr from '../translations/fr.js';
 import * as frBe from '../translations/fr-be.js';
 import * as frFr from '../translations/fr-fr.js';
+import * as es from '../translations/es.js';
+import * as it from '../translations/it.js';
+import * as deAt from '../translations/de-AT.js';
 import { DOCUMENTS, TAGS } from './document-fetcher';
 
 const newsPagePrefixes = {
@@ -21,6 +24,18 @@ const newsPagePrefixes = {
     frFr.default['news-page-prefix'].body?.static
     || frFr.default['news-page-prefix'].b?.s
     || frFr.default['news-page-prefix'],
+  es:
+    es.default['news-page-prefix'].body?.static
+    || es.default['news-page-prefix'].b?.s
+    || es.default['news-page-prefix'],
+  it:
+    it.default['news-page-prefix'].body?.static
+    || it.default['news-page-prefix'].b?.s
+    || it.default['news-page-prefix'],
+  'de-AT':
+    deAt.default['news-page-prefix'].body?.static
+    || deAt.default['news-page-prefix'].b?.s
+    || deAt.default['news-page-prefix'],
 };
 
 export function linkResolver(doc) {
